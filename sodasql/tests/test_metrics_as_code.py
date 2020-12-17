0291fa7491221ab33dcd31b1e71859792436fa72
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2020-12-17 14:18:15 +0100

    Revert to original password

--- sodasql/tests/test_metrics_as_code.py
+++ sodasql/tests/test_metrics_as_code.py
@@ -26,8 +26,8 @@ class TestMetricsAsCode(AbstractScanTest):
             'type': 'postgres',
             'host': 'localhost',
             'port': '5432',
-            'username': 'sodasql',
-            'database': 'sodasql',
+            'username': 'sodalite',
+            'database': 'sodalite',
             'schema': 'public'})
 
     def setUp(self) -> None:
