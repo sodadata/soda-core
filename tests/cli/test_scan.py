cca7066bafc859664e8bd7a6bb0318bb5919b23e
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2020-12-28 16:50:35 +0100

    Fix cli test

--- tests/cli/test_scan.py
+++ tests/cli/test_scan.py
@@ -1,13 +1,13 @@
 from unittest import TestCase
 
 from click.testing import CliRunner
-from sodasql.cli.cli import soda_sql_cli
+from sodasql.cli import main
 
 
 class TestScan(TestCase):
 
     def test_scan(self):
         runner = CliRunner()
-        result = runner.invoke(soda_sql_cli, ['scan', '/tmp', 'postgres', 'test_table'])
+        result = runner.invoke(main, ['scan', '/tmp', 'postgres', 'test_table'])
         self.assertEqual(result.exit_code, 0)
         self.assertIn('Scan dir=/tmp store=postgres table=test_table', result.output)
