f2b275358e6ba547809f744f405f4070fab94523
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2020-12-31 16:25:32 +0100

    Refactoring, Updated docs

--- tests/cli/test_scan_command.py
+++ tests/cli/test_scan_command.py
@@ -1,12 +1,4 @@
-from unittest import TestCase
-from unittest.mock import patch
-from click.testing import CliRunner
-import logging
-from unittest import skip
-
-from sodasql.cli import main
 from .cli_base_test import BaseTestCase
-from sodasql.cli.utils import ProfilesReader
 
 
 class TestScan(BaseTestCase):
@@ -22,11 +14,8 @@ class TestScan(BaseTestCase):
                                 "(null,     null)"])
 
     def test_scan_command(self):
-        with patch.object(ProfilesReader, 'PROFILES_FILE_PATH', self.test_profiles_file):
-            runner = CliRunner()
-            result = runner.invoke(main, ['scan',
-                                          self._get_test_resource_path('test_scan_configuration'),
-                                          self.test_warehouse,
-                                          self.test_table])
-            self._log_result(result)
-            self.assertEqual(result.exit_code, 0)
+        result = self._execute_cli(['scan',
+                                   self._get_test_resource_path('test_project'),
+                                   self.test_warehouse,
+                                   self.test_table])
+        self.assertEqual(result.exit_code, 0)
--- tests/cli/test_scan_configuration_reader.py
+++ tests/cli/test_scan_configuration_reader.py
@@ -12,6 +12,6 @@ class TestScanConfigurationReader(BaseTestCase):
     def test_scan_configuration_reader(self):
         reader = ScanConfigurationReader('test_warehouse',
                                          'test_table',
-                                         self._get_test_resource_path('test_scan_configuration'))
+                                         self._get_test_resource_path('test_project'))
         self._assert_logs(reader.parse_logs)
         self.assertListEqual(sorted(list(reader.configuration.metrics)), ['max_length', 'min_length'])
