9896a42b98da702470f89a689788782c1d0b439d
Marcio Gualtieri <marcio.gualtieri@gmail.com>
2021-01-14 08:56:43 +0100

    Refactoring

--- .github/scripts/utils/slack.py
+++ .github/scripts/utils/slack.py
@@ -12,17 +12,17 @@ class SlackMessageSender:
     slack_webhook_url: str
     branch: str
     ctx: ssl.SSLContext
-    branches_to_notify = ['master', 'main']
+    include_branches = ENV.get_included_branches().split()
 
     def __init__(self):
         self.slack_webhook_url = ENV.get_variable('SLACK_WEBHOOK_URL')
-        self.force_send = os.environ.get('FORCE_SEND', 'false')
+        self.force_send = ENV.get_force_send()
         self.ctx = SlackMessageSender._create_non_verifying_context()
         self.branch = ENV.get_branch()
 
     def send_slack_message(self, msg: str):
         payload = {"text": msg}
-        if (self.branch in self.branches_to_notify) or self.force_send == "true":
+        if (self.branch in self.included_branches) or self.force_send == "true":
             response = requests.post(self.slack_webhook_url, data=json.dumps(payload),
                                      headers={'Content-Type': 'application/json'})
             if response.status_code != 200:
