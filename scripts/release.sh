b97cad50c151eedf201e3b5d5c07cb7055848991
tombaeyens <mail@tombaeyens.be>
2021-02-01 18:07:17 +0100

    Remove quotes from insert when recreating table in tests

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,8 +2,8 @@
 
 # Run this from the root project dir with scripts/run_all_tests.sh
 
-VERSION=2.0.0b4
-MESSAGE="2.0.0b4 short summary of changes"
+VERSION=2.0.0b5
+MESSAGE="Fixed Snowflake CLI issue #65"
 
 git tag -a $VERSION -m $MESSAGE
 git push origin $VERSION
