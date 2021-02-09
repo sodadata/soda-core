c8b361e2c44abe00cacb1adbbf279756c7504efd
Milan Aleksić <milanaleksic@gmail.com>
2021-02-09 09:23:11 +0100

    mark development version '0.0.0' so that we can fetch it

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_version():
     with open(path.join("sodasql", "version.py")) as f:
         contents = f.read()
         matches = re.findall(r"SODA_SQL_VERSION = '([^']+)'", contents)
-        return matches[0] if len(matches) > 0 else '__dev__'
+        return matches[0] if len(matches) > 0 else '0.0.0'
 
 
 setup(
