e1705a4786cb533b2b676771e14af4457aa592e6
Dirk Groenen <dirk@bitlabs.nl>
2021-04-08 18:11:00 +0200

    Remove docs sodadata/docs#1 (#255)

--- scripts/serve_docs_locally.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Starting http server on http://localhost:3000/ and opening browser"
-echo "Kill this process (CTRL+C) to stop serving the docs"
-echo "This uses docsify. Install it if you don't aready have it"
-(sleep 1; open http://localhost:3000/) &
-SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-docsify serve $SCRIPTS_DIR/../docs
