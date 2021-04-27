927c1d04e343af047c85fb28fba797c5e67de8fe
Vijay Kiran <mail@vijaykiran.com>
2021-04-27 23:28:05 +0200

    validity_format should only be applied to text columns
    Closes #279

---
+++ tests/local/warehouse/metrics/test_validity.py
@@ -0,0 +1,106 @@
+#  Copyright 2020 Soda
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#   http://www.apache.org/licenses/LICENSE-2.0
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+from sodasql.scan.metric import Metric
+from sodasql.scan.scan_yml_parser import KEY_COLUMNS, KEY_METRIC_GROUPS
+from tests.common.sql_test_case import SqlTestCase
+
+
+class TestValidity(SqlTestCase):
+
+    def test_text_column_validity(self):
+        self.sql_recreate_table(
+            [f"score {self.dialect.data_type_varchar_255}"],
+            ["('1')",
+             "('2')",
+             "('2')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('4')",
+             "('4')",
+             "('5')",
+             "(null)"])
+
+        scan_result = self.scan({
+            KEY_METRIC_GROUPS: [
+                Metric.METRIC_GROUP_VALIDITY
+            ],
+            KEY_COLUMNS: {
+                'score': {
+                    'valid_format': 'number_whole'
+                }
+            },
+        })
+
+        self.assertEqual(scan_result.get(Metric.VALID_COUNT, 'score'), 11)
+
+    def test_numeric_column_validity_format(self):
+        self.sql_recreate_table(
+            [f"score {self.dialect.data_type_integer}"],
+            ["(1)",
+             "(2)",
+             "(2)",
+             "(3)",
+             "(3)",
+             "(3)",
+             "(3)",
+             "(3)",
+             "(4)",
+             "(4)",
+             "(5)",
+             "(null)"])
+
+        scan_result = self.scan({
+            KEY_METRIC_GROUPS: [
+                Metric.METRIC_GROUP_VALIDITY
+            ],
+            KEY_COLUMNS: {
+                'score': {
+                    'valid_format': 'number_whole'
+                }
+            },
+        })
+        with self.assertRaises(AssertionError):
+            scan_result.get(Metric.VALID_COUNT, 'score')
+
+    def test_numeric_column_validity_without_format(self):
+        self.sql_recreate_table(
+            [f"score {self.dialect.data_type_varchar_255}"],
+            ["('1')",
+             "('2')",
+             "('2')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('3')",
+             "('4')",
+             "('4')",
+             "('5')",
+             "(null)"])
+
+        scan_result = self.scan({
+            KEY_METRIC_GROUPS: [
+                Metric.METRIC_GROUP_VALIDITY
+            ],
+            KEY_COLUMNS: {
+                'score': {
+                    'valid_format': 'number_whole'
+                }
+            }
+        })
+        self.assertEqual(scan_result.get(Metric.INVALID_COUNT), 0)
+        self.assertEqual(scan_result.get(Metric.INVALID_PERCENTAGE), 0.0)
+        self.assertEqual(scan_result.get(Metric.VALID_COUNT), 11)
+        self.assertEqual(scan_result.get(Metric.VALID_PERCENTAGE), 91.66666666666667)
