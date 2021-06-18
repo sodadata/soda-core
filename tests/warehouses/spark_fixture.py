9f628982653f2ca9e75e007cdcec9e57d4d7d44c
Cor Zuurmond <jczuurmond@protonmail.com>
2021-06-18 16:06:55 +0200

    Add spark warehouse fixture

---
+++ tests/warehouses/spark_fixture.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from tests.common.warehouse_fixture import WarehouseFixture
+
+
+class SparkFixture(WarehouseFixture):
+
+    def create_database(self):
+        pass
+
+    def drop_database(self):
+        pass
+
+    def tear_down(self):
+        pass
