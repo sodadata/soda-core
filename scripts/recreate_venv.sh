c5670516e3c2ab0c9fb2483367b444e2959598a5
Milan Aleksić <milanaleksic@gmail.com>
2021-07-07 11:41:48 +0200

    make the recreate_venv.sh script work again

--- scripts/recreate_venv.sh
+++ scripts/recreate_venv.sh
@@ -13,8 +13,6 @@ python3 -m venv $VENV_DIR
 source $VENV_DIR/bin/activate
 pip install --upgrade pip
 pip install "$(cat dev-requirements.in | grep pip-tools)"
-pip-compile requirements.in
 pip-compile dev-requirements.in
-pip install -r requirements.txt
 pip install -r dev-requirements.txt
-pip install -e .
+pip install -r requirements.txt
