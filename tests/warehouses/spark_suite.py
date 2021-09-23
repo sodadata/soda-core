af29e8366813c529a440301a9d0468e391f09f71
Cor Zuurmond <jczuurmond@protonmail.com>
2021-09-23 11:19:19 +0200

    Let test also use ColumnMetadata

--- tests/warehouses/spark_suite.py
+++ tests/warehouses/spark_suite.py
@@ -1,3 +1,5 @@
+from sodasql.dialects.spark_dialect import ColumnMetadata
+
 from tests.common.sql_test_case import TARGET_SPARK
 from tests.common.sql_test_suite import SqlTestSuite
 
@@ -9,9 +11,13 @@ class SparkSuite(SqlTestSuite):
         super().setUp()
 
     def test_sql_columns_metadata(self):
-        expected = [("name", self.dialect.data_type_varchar_255.lower(), "YES")]
+        data_type = self.dialect.data_type_varchar_255.lower()
+        expected = [
+            ColumnMetadata("name", data_type, is_nullable="YES")
+        ]
 
         self.sql_recreate_table([" ".join(column[:2]) for column in expected])
-        columns_metadata = self.dialect.sql_columns_metadata(self.default_test_table_name)
+        columns_metadata = self.dialect.sql_columns_metadata(
+            self.default_test_table_name)
 
         assert columns_metadata == expected
