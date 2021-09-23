5dae4dc84cb8b080704888f2fd57ac03bd09d32d
Cor Zuurmond <jczuurmond@protonmail.com>
2021-09-23 10:26:38 +0200

    Add test for sql columns metadata

--- tests/warehouses/spark_suite.py
+++ tests/warehouses/spark_suite.py
@@ -7,3 +7,11 @@ class SparkSuite(SqlTestSuite):
     def setUp(self) -> None:
         self.target = TARGET_SPARK
         super().setUp()
+
+    def test_sql_columns_metadata(self):
+        expected = [("name", self.dialect.data_type_varchar_255.lower(), "YES")]
+
+        self.sql_recreate_table([" ".join(column[:2]) for column in expected])
+        columns_metadata = self.dialect.sql_columns_metadata(self.default_test_table_name)
+
+        assert columns_metadata == expected
