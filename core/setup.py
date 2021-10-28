a7d451bff27cc0531bba628373f8aa97c16cde71
Ayoub Fakir <ayoub@fakir.dev>
2021-10-28 13:41:25 +0200

    # This is a combination of 2 commits. # This is the 1st commit message:
    add luddite requirement to setup.py
    
    # The commit message #2 will be skipped:
    
    # Add warning when user is using an old version

--- core/setup.py
+++ core/setup.py
@@ -23,7 +23,8 @@ requires = [
     'cryptography==3.3.2',
     'pyyaml>=5.4.1, <6.0',
     'requests>=2.23.0, <3.0',
-    'Deprecated>=1.2.13, <1.3'
+    'Deprecated>=1.2.13, <1.3',
+    'luddite==1.0.2'
 ]
 # TODO Fix the params
 # TODO Add a warning that installing core doesn't give any warehouse functionality
