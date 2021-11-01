63116f7635a833fcb0be2b59f0377073858b8e6c
fakirAyoub <ayoub@fakir.dev>
2021-11-01 12:01:06 +0100

    suppress exception to not break CLI test

--- core/sodasql/scan/scan_builder.py
+++ core/sodasql/scan/scan_builder.py
@@ -74,12 +74,8 @@ class ScanBuilder:
         self.assert_no_warnings_or_errors = True
         self.soda_server_client: SodaServerClient = None
         logger = logging.getLogger(__name__)
-        latest_version = ""
-        try:
+        with suppress(Exception):
             latest_version = luddite.get_version_pypi('soda-sql-core')
-        except:
-            logger.warning("Cannot check Soda SQL version.")
-            pass
         if SODA_SQL_VERSION != latest_version:
             logger.warning(f"You are using an old soda-sql version: {SODA_SQL_VERSION}, "
                            f"please upgrade to the latest one: {latest_version}")
