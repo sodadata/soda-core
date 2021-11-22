e106f29d42e70ad327dfc3a347421c3580a7fb42
Ayoub Fakir <ayoub@fakir.dev>
2021-11-22 14:04:08 +0100

    fix date_eu test - revert to previous state

--- tests/local/warehouse/validity/test_date_and_time_validity_formats.py
+++ tests/local/warehouse/validity/test_date_and_time_validity_formats.py
@@ -8,10 +8,9 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-import logging
+
 from datetime import datetime, timezone
 
-import pytest
 from sodasql.scan.metric import Metric
 from sodasql.scan.scan_yml_parser import KEY_METRICS, KEY_COLUMNS
 from tests.common.sql_test_case import SqlTestCase
@@ -23,7 +22,6 @@ class TestDateAndTimeValidityFormats(SqlTestCase):
         self.sql_recreate_table(
             [f"name {self.dialect.data_type_varchar_255}"],
             ["('21-01-2021')",
-             "('01/08/2020')",
              "('21.01.2021')",
              "('21/01/2021')",
              "('21/01/21')",
@@ -149,35 +147,32 @@ class TestDateAndTimeValidityFormats(SqlTestCase):
         self.assertEqual(scan_result.get(Metric.VALID_PERCENTAGE, 'name'), 62.5)
 
     def test_date_iso_8601(self):
-        if self.dialect.type == 'sqlserver':
-            pass
-        else:
+        test_date = datetime.now()
+        test_date_with_timezone = datetime.now(timezone.utc)
+        self.sql_recreate_table(
+            [f"name {self.dialect.data_type_varchar_255}"],
+            [f"('{test_date.isoformat()}')",
+             f"('{test_date_with_timezone.isoformat()}')",
+             "('2021, January 21')",
+             "('October 21, 2015')",
+             "(null)"])
 
-            test_date = datetime.now()
-            test_date_with_timezone = datetime.now(timezone.utc)
-            self.sql_recreate_table(
-                [f"name {self.dialect.data_type_varchar_255}"],
-                [f"('{test_date.isoformat()}')",
-                 f"('{test_date_with_timezone.isoformat()}')",
-                 "('2021, January 21')",
-                 "('October 21, 2015')",
-                 "(null)"])
-
-            scan_result = self.scan({
-                KEY_METRICS: [
-                    Metric.INVALID_COUNT,
-                    Metric.INVALID_PERCENTAGE,
-                    Metric.VALID_COUNT,
-                    Metric.VALID_PERCENTAGE,
-                ],
-                KEY_COLUMNS: {
-                    'name': {
-                        'valid_format': 'date_iso_8601'
-                    }
+        scan_result = self.scan({
+            KEY_METRICS: [
+                Metric.INVALID_COUNT,
+                Metric.INVALID_PERCENTAGE,
+                Metric.VALID_COUNT,
+                Metric.VALID_PERCENTAGE,
+            ],
+            KEY_COLUMNS: {
+                'name': {
+                    'valid_format': 'date_iso_8601'
                 }
-            })
+            }
+        })
 
-            self.assertEqual(scan_result.get(Metric.INVALID_COUNT, 'name'), 2)
-            self.assertEqual(scan_result.get(Metric.INVALID_PERCENTAGE, 'name'), 40.0)
-            self.assertEqual(scan_result.get(Metric.VALID_COUNT, 'name'), 2)
-            self.assertEqual(scan_result.get(Metric.VALID_PERCENTAGE, 'name'), 40.0)
+        self.assertEqual(scan_result.get(Metric.VALUES_COUNT, 'name'), 4)
+        self.assertEqual(scan_result.get(Metric.INVALID_COUNT, 'name'), 2)
+        self.assertEqual(scan_result.get(Metric.INVALID_PERCENTAGE, 'name'), 40.0)
+        self.assertEqual(scan_result.get(Metric.VALID_COUNT, 'name'), 2)
+        self.assertEqual(scan_result.get(Metric.VALID_PERCENTAGE, 'name'), 40.0)
