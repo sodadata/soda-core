66fcab69de8bb74cb02cb77dc177d5778866d96c
Vijay Kiran <mail@vijaykiran.com>
2022-01-14 14:44:57 +0100

    Bump to 2.1.3

--- packages/bigquery/setup.py
+++ packages/bigquery/setup.py
@@ -8,7 +8,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-sql-bigquery"
-package_version = '2.1.2'
+package_version = '2.1.3'
 # TODO Add proper description
 description = "Soda SQL BigQuery"
 
