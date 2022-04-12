282a378ae8105388fcfb7137c7673237c27933c8
Vijay Kiran <mail@vijaykiran.com>
2022-04-12 10:05:19 +0100

    Bump to 3.0.0b6

--- soda/postgres/setup.py
+++ soda/postgres/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-postgres"
-package_version = "3.0.0b5"
+package_version = "3.0.0b6"
 # TODO Add proper description
 description = "Soda Core Postgres Package"
 
