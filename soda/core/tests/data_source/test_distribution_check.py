27fe41b18122f80c257a5b688fea4372d5a63ecf
Milan Lukac <milan@lukac.online>
2022-05-03 11:58:51 -0700

    Disable scientific tests on standard test run

--- soda/core/tests/data_source/test_distribution_check.py
+++ soda/core/tests/data_source/test_distribution_check.py
@@ -1,11 +1,14 @@
 import os
 from textwrap import dedent
 
+import pytest
+
 from tests.helpers.common_test_tables import customers_dist_check_test_table
 from tests.helpers.mock_file_system import MockFileSystem
 from tests.helpers.scanner import Scanner
 
-
+# TOOO: unskip after scientific package is re-enabled and/or move to someplace else.
+@pytest.mark.skip
 def test_distribution_check(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_dist_check_test_table)
 
