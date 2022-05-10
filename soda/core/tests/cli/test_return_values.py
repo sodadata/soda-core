9e79bd564409cfa8d97a54c881cd45b2627f40d3
Milan Lukac <milan@lukac.online>
2022-05-10 17:39:02 +0200

    simplified test + test code cleanup

--- soda/core/tests/cli/test_return_values.py
+++ soda/core/tests/cli/test_return_values.py
@@ -1,12 +1,31 @@
 from textwrap import dedent
+import pytest
 
 from tests.cli.run_cli import run_cli
 from tests.helpers.common_test_tables import customers_test_table
 from tests.helpers.mock_file_system import MockFileSystem
 from tests.helpers.scanner import Scanner
-from tests.conftest import test_data_source, data_source_config_str
+from tests.conftest import test_data_source
 
 
+def get_ds_definition():
+    return dedent(
+        """
+        data_source cli_ds:
+            type: postgres
+            connection:
+                host: localhost
+                username: sodasql
+            database: sodasql
+            schema: public
+        """
+    ).strip()
+
+
+@pytest.mark.skipif(
+    test_data_source != "postgres",
+    reason="Run for postgres only as nothing data source specific is tested..",
+)
 def test_non_existing_files(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_test_table)
 
@@ -14,7 +33,7 @@ def test_non_existing_files(scanner: Scanner):
         [
             "scan",
             "-d",
-            test_data_source,
+            "cli_ds",
             "-c",
             "non-existing.yml",
             "checks.yml",
@@ -23,13 +42,17 @@ def test_non_existing_files(scanner: Scanner):
     assert result.exit_code == 3
 
 
-def test_ok_with_variable(scanner: Scanner, mock_file_system: MockFileSystem, data_source_config_str: str):
+@pytest.mark.skipif(
+    test_data_source != "postgres",
+    reason="Run for postgres only as nothing data source specific is tested..",
+)
+def test_ok_with_variable(scanner: Scanner, mock_file_system: MockFileSystem):
     table_name = scanner.ensure_test_table(customers_test_table)
 
     user_home_dir = mock_file_system.user_home_dir()
 
     mock_file_system.files = {
-        f"{user_home_dir}/configuration.yml": data_source_config_str,
+        f"{user_home_dir}/configuration.yml": get_ds_definition(),
         f"{user_home_dir}/checks.yml": dedent(
             f"""
                 checks for {table_name}:
@@ -42,7 +65,7 @@ def test_ok_with_variable(scanner: Scanner, mock_file_system: MockFileSystem, da
         [
             "scan",
             "-d",
-            test_data_source,
+            "cli_ds",
             "-c",
             "configuration.yml",
             "-v",
@@ -53,13 +76,17 @@ def test_ok_with_variable(scanner: Scanner, mock_file_system: MockFileSystem, da
     assert result.exit_code == 0
 
 
-def test_fail(scanner: Scanner, mock_file_system: MockFileSystem, data_source_config_str: str):
+@pytest.mark.skipif(
+    test_data_source != "postgres",
+    reason="Run for postgres only as nothing data source specific is tested..",
+)
+def test_fail(scanner: Scanner, mock_file_system: MockFileSystem):
     table_name = scanner.ensure_test_table(customers_test_table)
 
     user_home_dir = mock_file_system.user_home_dir()
 
     mock_file_system.files = {
-        f"{user_home_dir}/configuration.yml": data_source_config_str,
+        f"{user_home_dir}/configuration.yml": get_ds_definition(),
         f"{user_home_dir}/checks.yml": dedent(
             f"""
                 checks for {table_name}:
@@ -72,7 +99,7 @@ def test_fail(scanner: Scanner, mock_file_system: MockFileSystem, data_source_co
         [
             "scan",
             "-d",
-            test_data_source,
+            "cli_ds",
             "-c",
             "configuration.yml",
             "checks.yml",
@@ -81,13 +108,17 @@ def test_fail(scanner: Scanner, mock_file_system: MockFileSystem, data_source_co
     assert result.exit_code == 2
 
 
-def test_warn(scanner: Scanner, mock_file_system: MockFileSystem, data_source_config_str: str):
+@pytest.mark.skipif(
+    test_data_source != "postgres",
+    reason="Run for postgres only as nothing data source specific is tested..",
+)
+def test_warn(scanner: Scanner, mock_file_system: MockFileSystem):
     table_name = scanner.ensure_test_table(customers_test_table)
 
     user_home_dir = mock_file_system.user_home_dir()
 
     mock_file_system.files = {
-        f"{user_home_dir}/configuration.yml": data_source_config_str,
+        f"{user_home_dir}/configuration.yml": get_ds_definition(),
         f"{user_home_dir}/checks.yml": dedent(
             f"""
                 checks for {table_name}:
@@ -101,7 +132,7 @@ def test_warn(scanner: Scanner, mock_file_system: MockFileSystem, data_source_co
         [
             "scan",
             "-d",
-            test_data_source,
+            "cli_ds",
             "-c",
             "configuration.yml",
             "checks.yml",
--- soda/core/tests/conftest.py
+++ soda/core/tests/conftest.py
@@ -1,13 +1,14 @@
 from __future__ import annotations
-import json
-
 import logging
 import os
 from typing import Any
-
+from dotenv import load_dotenv
+from soda.scan import Scan
 import pytest
 from soda.common.file_system import FileSystemSingleton
 from soda.common.logs import configure_logging
+from soda.configuration.configuration_parser import ConfigurationParser
+from soda.execution.data_source import DataSource
 from soda.telemetry.soda_telemetry import SodaTelemetry
 
 # Initialize telemetry in test mode. This is done before importing Scanner which initializes telemetry in standard mode so that we avoid unnecessary setup and re-setup which causes errors.
@@ -17,6 +18,9 @@ from tests.helpers.mock_file_system import MockFileSystem
 from tests.helpers.scanner import Scanner
 
 logger = logging.getLogger(__name__)
+
+# Load local env file so that test data sources can be set up.
+load_dotenv(".env", override=True)
 
 # In global scope because it is used in pytest annotations, it would not work as a fixture.
 test_data_source = os.getenv("test_data_source", "postgres")
@@ -42,9 +46,30 @@ def data_source_config_str() -> str:
 
 
 @pytest.fixture(scope="session")
-def data_source_config_dict(data_source_config_str) -> dict:
-    """Whole test data source config as string."""
-    return json.loads(data_source_config_str)
+def scan(data_source_config_str: str) -> Scan:
+    data_source_name = test_data_source
+
+    scan = Scan()
+    scan.set_data_source_name(data_source_name)
+    scan.add_configuration_yaml_str(data_source_config_str)
+
+    return scan
+
+
+@pytest.fixture(scope="session")
+def data_source(scan: Scan) -> DataSource:
+    data_source_name = test_data_source
+
+    data_source_connection_manager = scan._data_source_manager
+    data_source = data_source_connection_manager.get_data_source(data_source_name)
+    if not data_source:
+        raise Exception(f"Unable to find and/or set up specified '{data_source_name}' test data_source config.")
+    connection = data_source_connection_manager.connect(data_source)
+    scan._get_or_create_data_source_scan(test_data_source)
+
+    yield data_source
+
+    connection.close()
 
 
 @pytest.fixture(scope="session")
@@ -71,3 +96,8 @@ def mock_file_system():
     FileSystemSingleton.INSTANCE = MockFileSystem()
     yield FileSystemSingleton.INSTANCE
     FileSystemSingleton.INSTANCE = original_file_system
+
+
+def format_query_one_line(query: str) -> str:
+    """@TODO: implement"""
+    return query
