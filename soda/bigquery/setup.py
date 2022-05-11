0fa27d7c2e25c791c1b17d5c0044a23a1d45db8a
Vijay Kiran <mail@vijaykiran.com>
2022-05-11 16:10:56 +0200

    Bump to 3.0.0b11

--- soda/bigquery/setup.py
+++ soda/bigquery/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 8):
     sys.exit(1)
 
 package_name = "soda-core-bigquery"
-package_version = "3.0.0b10"
+package_version = "3.0.0b11"
 description = "Soda Core Bigquery Package"
 
 requires = [
