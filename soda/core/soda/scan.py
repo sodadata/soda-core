672442db83bbb591aa3417196b00df7174b59ec6
Bastien Boutonnet <bastien.b1@gmail.com>
2022-05-12 12:59:38 +0200

    fix identation issue from conflict resolution

--- soda/core/soda/scan.py
+++ soda/core/soda/scan.py
@@ -413,28 +413,25 @@ class Scan:
             self._close()
         return exit_value
 
-
     def run_automated_monitoring(self):
-            # this is where automated monitoring is called
-            for data_source_scan in self._data_source_scans:
-                for monitoring_cfg in data_source_scan.data_source_scan_cfg.monitoring_cfgs:
-                    data_source_name = data_source_scan.data_source_scan_cfg.data_source_name
-                    data_source_scan = self._get_or_create_data_source_scan(data_source_name)
-                    if data_source_scan:
-                        if self._is_experimental_auto_monitoring:
-                            monitor_runner = data_source_scan.create_automated_monitor_run(monitoring_cfg, self)
-                            monitor_runner.run()
-                            self._is_automated_monitoring_run = True
-                        else:
-                            self._logs.info("Automated monitoring feature is not implemented yet. Stay tuned!")
+        # this is where automated monitoring is called
+        for data_source_scan in self._data_source_scans:
+            for monitoring_cfg in data_source_scan.data_source_scan_cfg.monitoring_cfgs:
+                data_source_name = data_source_scan.data_source_scan_cfg.data_source_name
+                data_source_scan = self._get_or_create_data_source_scan(data_source_name)
+                if data_source_scan:
+                    if self._is_experimental_auto_monitoring:
+                        monitor_runner = data_source_scan.create_automated_monitor_run(monitoring_cfg, self)
+                        monitor_runner.run()
+                        self._is_automated_monitoring_run = True
                     else:
-                        data_source_names = ", ".join(self._data_source_manager.data_source_properties_by_name.keys())
-                        self._logs.error(
-                            f"Could not run monitors on data_source {data_source_name} because It is not "
-                            f"configured: {data_source_names}"
-                        )
-        else:
-            self._logs.info("Automated monitoring feature is not implemented yet. Stay tuned!")
+                        self._logs.info("Automated monitoring feature is not implemented yet. Stay tuned!")
+                else:
+                    data_source_names = ", ".join(self._data_source_manager.data_source_properties_by_name.keys())
+                    self._logs.error(
+                        f"Could not run monitors on data_source {data_source_name} because It is not "
+                        f"configured: {data_source_names}"
+                    )
 
     def run_profile_columns(self):
         for data_source_scan in self._data_source_scans:
@@ -453,7 +450,6 @@ class Scan:
                         f"configured: {data_source_names}",
                         location=profile_columns_cfg.location,
                     )
-
 
     def __checks_to_text(self, checks: list[Check]):
         return "/n".join([str(check) for check in checks])
