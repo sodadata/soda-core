193e89002984b76b0a21477f780a4c39d3de9831
Bastien Boutonnet <bastien.b1@gmail.com>
2022-05-16 16:04:31 +0200

    feat: add table discovery (#1341)
    * add table discovery
    * add test expectation entry for redshift
    * add test entry for bq
    * update dtypes expectations for bq
    * add test case for include wildcard
    * add exclusion clause test
    
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

---
+++ soda/core/soda/sodacl/discover_tables_cfg.py
@@ -0,0 +1,11 @@
+from typing import List
+
+from soda.sodacl.location import Location
+
+
+class DiscoverTablesCfg:
+    def __init__(self, data_source_name: str, location: Location):
+        self.data_source_name: str = data_source_name
+        self.location: Location = location
+        self.include_tables: List[str] = []
+        self.exclude_tables: List[str] = []
