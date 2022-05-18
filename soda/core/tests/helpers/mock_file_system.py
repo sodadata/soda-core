7df9fce97cd2806269e71e5cfcb8e6435a6ca8f9
tombaeyens <mail@tombaeyens.be>
2022-05-18 17:25:46 +0200

    #1353 SODA-494 Fixing recursive loading of files

--- soda/core/tests/helpers/mock_file_system.py
+++ soda/core/tests/helpers/mock_file_system.py
@@ -6,7 +6,7 @@ from soda.common.file_system import FileSystem, FileSystemSingleton
 class MockFileSystem(FileSystem):
 
     current_dir = "/Users/johndoe"
-    dirs = set()
+    dirs = set('/')
     files = {}
 
     def __init__(self):
@@ -15,7 +15,7 @@ class MockFileSystem(FileSystem):
     def user_home_dir(self):
         return "/Users/johndoe"
 
-    def mkdirs(self, path: str):
+    def mkdirs(self, path: str, absolute: bool = False):
         normalized_path = self.normalize(path)
         self.dirs.add(normalized_path)
 
@@ -26,10 +26,6 @@ class MockFileSystem(FileSystem):
     def file_write_from_str(self, path: str, file_content_str):
         normalized_path = self.normalize(path)
         self.files[normalized_path] = file_content_str
-
-    def list_dir(self, dir_path):
-        normalized_dir_path = self.normalize(dir_path)
-        return {k: v for k, v in self.files.items() if k.startswith(normalized_dir_path)}
 
     def exists(self, path: str):
         normalized_path = self.normalize(path)
@@ -43,16 +39,20 @@ class MockFileSystem(FileSystem):
         normalized_path = self.normalize(path)
         return normalized_path in self.dirs
 
-    def is_readable(self, path: str):
-        return self.is_file(path)
-
-    def is_readable_file(self, file_path: str):
-        return self.is_file(file_path)
-
     def expand_user(self, path: str):
         if path.startswith("~"):
             return self.user_home_dir() + path[1:]
         return path
+
+    def scan_dir(self, dir_path: str):
+        return super().scan_dir(dir_path)
+
+    def dirname(self, path: str):
+        return super().dirname(path)
+
+    def list_dir(self, dir_path):
+        normalized_dir_path = self.normalize(dir_path)
+        return {k: v for k, v in self.files.items() if k.startswith(normalized_dir_path)}
 
     def normalize(self, path: str):
         if path.startswith("~"):
---
+++ soda/core/tests/unit/test_collect_files.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import logging
+from typing import List
+
+from soda.scan import Scan
+from tests.conftest import project_root_dir
+
+
+basedir = f"{project_root_dir}soda/core/tests/unit/test_collect_files_dir"
+
+
+def collect_paths(path: str, recursive: bool | None = True, suffix: str | None = ".yml") -> List[str]:
+    scan = Scan()
+    return scan._collect_file_paths(path=path, recursive=recursive, suffix=suffix)
+
+
+def assert_paths(actual_paths, expected_file_paths):
+    prefix_length = len(basedir)
+    relative_actual_paths = [path[prefix_length:] for path in actual_paths]
+    assert relative_actual_paths == expected_file_paths
+
+
+def test_collect_files():
+    assert_paths(collect_paths(f"{basedir}/cfgs"), [
+        "/cfgs/nested/nested/nested_configuration.yml",
+        "/cfgs/nested/nested/nested_configuration2.yml",
+        "/cfgs/root_configuration.yml",
+    ])
+
+
+def test_collect_files_slash():
+    assert_paths(collect_paths(f"{basedir}/sodacls/"), [
+        "/sodacls/nested/nested_checks.yml",
+        "/sodacls/root_checks.yml",
+        "/sodacls/root_checks2.yml",
+    ])
+
+
+def test_collect_files_empty():
+    assert_paths(collect_paths(f"{basedir}/empty"), [])
