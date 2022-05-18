91dd4c8fd02f18f1992eaca6a14ba096e094ac27
Baturay Ofluoglu <baturayofluoglu@gmail.com>
2022-05-18 16:13:40 +0200

    Soda 159 - Test anomaly detection for nested metrics (#1354)
    * Test anomaly detection for numeric metrics
    * Test anomaly check with nested metrics
    
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/core/tests/data_source/test_anomaly_check.py
+++ soda/core/tests/data_source/test_anomaly_check.py
@@ -104,3 +104,53 @@ def test_anomaly_detection_pass_numeric_metrics(numeric_metric, column, scanner)
 
     scan.execute()
     scan.assert_all_checks_pass()
+
+
+def test_anomaly_detection_missing_values(scanner):
+    import numpy as np
+
+    np.random.seed(61)
+    table_name = scanner.ensure_test_table(customers_test_table)
+
+    scan = scanner.create_test_scan()
+
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+              - anomaly score for missing_count(id) < default:
+                    missing values: ["ID2"]
+        """
+    )
+
+    scan.mock_historic_values(
+        metric_identity=f"metric-{scan._scan_definition_name}-{scan._data_source_name}-{table_name}-id-missing_count-1beec996",
+        metric_values=[10, 10, 10, 9, 8, 0, 0, 0, 0],
+    )
+
+    scan.execute()
+    scan.assert_all_checks_pass()
+
+
+def test_anomaly_detection_invalid_values(scanner):
+    import numpy as np
+
+    np.random.seed(61)
+    table_name = scanner.ensure_test_table(customers_test_table)
+
+    scan = scanner.create_test_scan()
+
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+              - anomaly score for invalid_count(id) < default:
+                    valid format: email
+        """
+    )
+
+    scan.mock_historic_values(
+        metric_identity=f"metric-{scan._scan_definition_name}-{scan._data_source_name}-{table_name}-id-invalid_count-61813b33",
+        metric_values=[10, 10, 10, 9, 8, 0, 0, 0, 0],
+    )
+
+    scan.execute()
+    scan.assert_all_checks_pass()
