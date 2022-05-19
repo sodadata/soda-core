e311b514231ddf0970fe1b80de2aa14a8c219bbc
Vijay Kiran <mail@vijaykiran.com>
2022-05-19 17:12:42 +0200

    Bump to 3.0.0b14

--- soda/snowflake/setup.py
+++ soda/snowflake/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 8):
     sys.exit(1)
 
 package_name = "soda-core-snowflake"
-package_version = "3.0.0b13"
+package_version = "3.0.0b14"
 description = "Soda Core Snowflake Package"
 
 requires = [
