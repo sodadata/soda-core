b4197c314cc5f8b35da4889ddbdbc26d405bc021
Tom Baeyens <mail@tombaeyens.be>
2022-05-23 13:53:50 +0200

    Fixing suffix scanning of configuration and check files (#1365)
    * Fixing suffix scanning of configuration and check files
    
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/core/tests/unit/test_collect_files.py
+++ soda/core/tests/unit/test_collect_files.py
@@ -6,9 +6,9 @@ from tests.conftest import project_root_dir
 basedir = f"{project_root_dir}soda/core/tests/unit/test_collect_files_dir"
 
 
-def collect_paths(path: str, recursive: bool | None = True, suffix: str | None = ".yml") -> list[str]:
+def collect_paths(path: str, recursive: bool | None = True, suffixes: list[str] | None = None) -> list[str]:
     scan = Scan()
-    return scan._collect_file_paths(path=path, recursive=recursive, suffix=suffix)
+    return scan._collect_file_paths(path=path, recursive=recursive, suffixes=suffixes)
 
 
 def assert_paths(actual_paths: iter, expected_file_paths: set):
@@ -34,7 +34,22 @@ def test_collect_files_slash():
         {
             "/sodacls/root_checks2.yml",
             "/sodacls/nested/nested_checks.yml",
-            "/sodacls/root_checks.yml",
+            "/sodacls/root_checks.yaml",
+        },
+    )
+
+
+def test_collect_files_yaml():
+    assert_paths(
+        collect_paths(f"{basedir}/sodacls/root_checks.yaml"),
+        {
+            "/sodacls/root_checks.yaml",
+        },
+    )
+    assert_paths(
+        collect_paths(f"{basedir}/cfgs/file_to_be_ignored.txt"),
+        {
+            "/cfgs/file_to_be_ignored.txt",
         },
     )
 
