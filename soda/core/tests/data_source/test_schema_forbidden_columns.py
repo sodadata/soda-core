184bd6b46a9835aca42b7d33768c4a97188da70d
Tom Baeyens <mail@tombaeyens.be>
2022-05-24 19:37:52 +0200

    Refactored to actual table and actual column names (#1370)
    * Refactored to actual table and actual column names
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixing actual name glitches
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added for each schema check (#1368)
    
    * Added for each schema check
    
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>
    
    * Bump to 3.0.0b15
    
    * Fixing suffix scanning of configuration and check files (#1365)
    
    * Fixing suffix scanning of configuration and check files
    
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>
    
    * merging main
    
    * actual_column_name: Removed method invocation
    
    * Renaming and cleanin up the actual names to default_casify...
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/core/tests/data_source/test_schema_forbidden_columns.py
+++ soda/core/tests/data_source/test_schema_forbidden_columns.py
@@ -5,7 +5,7 @@ from tests.helpers.scanner import Scanner
 
 def test_forbidden_columns_pass(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_test_table)
-    format_column_default = scanner.data_source.format_column_default
+    default_casify_column_name = scanner.data_source.default_casify_column_name
 
     scan = scanner.create_test_scan()
     scan.add_sodacl_yaml_str(
@@ -13,7 +13,7 @@ def test_forbidden_columns_pass(scanner: Scanner):
       checks for {table_name}:
         - schema:
             fail:
-              when forbidden column present: [{format_column_default('non_existing_column_one')}, {format_column_default('"%non_existing%"')}]
+              when forbidden column present: [{default_casify_column_name('non_existing_column_one')}, {default_casify_column_name('"%non_existing%"')}]
     """
     )
     scan.execute()
@@ -23,7 +23,8 @@ def test_forbidden_columns_pass(scanner: Scanner):
 
 def test_forbidden_columns_fail(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_test_table)
-    format_column_default = scanner.data_source.format_column_default
+
+    default_casify_column_name = scanner.data_source.default_casify_column_name
 
     scan = scanner.create_test_scan()
     scan.add_sodacl_yaml_str(
@@ -31,19 +32,20 @@ def test_forbidden_columns_fail(scanner: Scanner):
       checks for {table_name}:
         - schema:
             fail:
-              when forbidden column present: [{format_column_default('id')}, {format_column_default('non_existing_column_one')}, {format_column_default('"%non_existing%"')}]
+              when forbidden column present: [{default_casify_column_name('id')}, {default_casify_column_name('non_existing_column_one')}, {default_casify_column_name('"%non_existing%"')}]
     """
     )
     scan.execute()
 
     scan.assert_all_checks_fail()
     check: SchemaCheck = scan._checks[0]
-    assert sorted(check.schema_present_column_names) == sorted([format_column_default("id")])
+    assert sorted(check.schema_present_column_names) == sorted([default_casify_column_name("id")])
 
 
 def test_forbidden_columns_fail_matching_wildcard(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_test_table)
-    format_column_default = scanner.data_source.format_column_default
+
+    default_casify_column_name = scanner.data_source.default_casify_column_name
 
     scan = scanner.create_test_scan()
     scan.add_sodacl_yaml_str(
@@ -52,8 +54,8 @@ def test_forbidden_columns_fail_matching_wildcard(scanner: Scanner):
         - schema:
             fail:
               when forbidden column present:
-               - {format_column_default('size*')}
-               - {format_column_default('non_existing_column_two')}
+               - {default_casify_column_name('size*')}
+               - {default_casify_column_name('non_existing_column_two')}
     """
     )
     scan.execute()
@@ -61,13 +63,14 @@ def test_forbidden_columns_fail_matching_wildcard(scanner: Scanner):
     scan.assert_all_checks_fail()
     check: SchemaCheck = scan._checks[0]
     assert sorted(check.schema_present_column_names) == sorted(
-        [format_column_default("size"), format_column_default("sizeTxt")]
+        [default_casify_column_name("size"), default_casify_column_name("sizeTxt")]
     )
 
 
 def test_forbidden_columns_warn(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_test_table)
-    format_column_default = scanner.data_source.format_column_default
+
+    default_casify_column_name = scanner.data_source.default_casify_column_name
 
     scan = scanner.create_test_scan()
     scan.add_sodacl_yaml_str(
@@ -75,11 +78,11 @@ def test_forbidden_columns_warn(scanner: Scanner):
       checks for {table_name}:
         - schema:
             warn:
-              when forbidden column present: [{format_column_default('id')}, {format_column_default('non_existing_column_one')}, {format_column_default('"%non_existing%"')}]
+              when forbidden column present: [{default_casify_column_name('id')}, {default_casify_column_name('non_existing_column_one')}, {default_casify_column_name('"%non_existing%"')}]
     """
     )
     scan.execute()
 
     scan.assert_all_checks_warn()
     check: SchemaCheck = scan._checks[0]
-    assert sorted(check.schema_present_column_names) == sorted([format_column_default("id")])
+    assert sorted(check.schema_present_column_names) == sorted([default_casify_column_name("id")])
