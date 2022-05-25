40b5737d49995ccb83c041f621f117a363e134b9
tituskx <tituskarelex@gmail.com>
2022-05-25 13:31:50 +0200

    add unit test that asserts new query with limit is correct

--- soda/core/tests/data_source/test_distribution_check.py
+++ soda/core/tests/data_source/test_distribution_check.py
@@ -7,6 +7,7 @@ from tests.helpers.mock_file_system import MockFileSystem
 from tests.helpers.scanner import Scanner
 
 
+
 @pytest.mark.skip("Unskip after scientific package is re-enabled and/or move to someplace else.")
 def test_distribution_check(scanner: Scanner):
     table_name = scanner.ensure_test_table(customers_dist_check_test_table)
@@ -45,3 +46,55 @@ def test_distribution_check(scanner: Scanner):
     scan.add_sodacl_yaml_file(f"{user_home_dir}/the_distribution_check_file.yml")
     scan.execute()
     os.remove(ref_file)
+
+
+@pytest.mark.parametrize(
+    "table, expectation",
+    [
+        pytest.param(
+            customers_dist_check_test_table,
+            "SELECT \n  size \nFROM {table_name}\n LIMIT 1000000"
+        ),
+    ]
+)
+def test_distribution_sql(scanner: Scanner, table, expectation):
+    table_name = scanner.ensure_test_table(table)
+
+    # The following files are made available on the mocked file system:
+    mock_file_system = MockFileSystem()
+
+    # user_home_dir = mock_file_system.user_home_dir()
+    user_home_dir = os.path.expanduser("~")
+
+    mock_file_system.files = {
+        f"{user_home_dir}/the_distribution_check_file.yml": dedent(
+            f"""
+                checks for {table_name}:
+                  - distribution_difference(size, my_happy_ml_model_distribution) >= 0.05:
+                      distribution reference file: ./customers_size_distribution_reference.yml
+            """
+        ).strip(),
+    }
+
+    # TODO: do this logic at mock file system
+    ref_file = f"{user_home_dir}/customers_size_distribution_reference.yml"
+    with open(ref_file, "w") as f:
+        reference_table = f"""
+            table: {table_name}
+            column: size
+            method: continuous
+            distribution reference:
+                bins: [1, 2, 3]
+                weights: [0.5, 0.2, 0.3]
+        """
+        f.write(reference_table)
+
+    scan = scanner.create_test_scan()
+    scan._configuration.file_system = mock_file_system
+    scan.add_sodacl_yaml_file(f"{user_home_dir}/the_distribution_check_file.yml")
+    scan.execute()
+
+    assert scan._checks[0].query.sql == expectation.format(table_name=table_name)
+    os.remove(ref_file)
+
+
