8da7230e1b865fc3e4fe7c2f4d91da85cf8d5e9e
tombaeyens <mail@tombaeyens.be>
2022-06-09 15:57:59 +0200

    precommit cleanup

--- soda/core/soda/sodacl/freshness_check_cfg.py
+++ soda/core/soda/sodacl/freshness_check_cfg.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import Optional
 
 from soda.sodacl.check_cfg import CheckCfg
 from soda.sodacl.location import Location
