16f12a9c6c0a88d083230b48d9ff8f5ae06399ca
baturayo <baturayofluoglu@gmail.com>
2022-06-13 16:13:28 +0300

    add configs for profile columns cfg

--- soda/core/soda/sodacl/data_source_check_cfg.py
+++ soda/core/soda/sodacl/data_source_check_cfg.py
@@ -21,7 +21,10 @@ class ProfileColumnsCfg(DataSourceCheckCfg):
         super().__init__(data_source_name, location)
         self.include_columns: List[str] = []
         self.exclude_columns: List[str] = []
-
+        # TODO add parsing for this configuration
+        self.limit_mins_maxs: int = 5
+        # TODO add parsing for this configuration
+        self.limit_frequent_values: int = 10
 
 class DiscoverTablesCfg(DataSourceCheckCfg):
     def __init__(self, data_source_name: str, location: Location):
