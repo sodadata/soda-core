964d7ec16f55cfd977cab74b7a3bc0f3325cdb93
Baturay Ofluoglu <baturayofluoglu@gmail.com>
2022-06-20 15:21:04 +0300

    refactor: use filesystem abstractions in distribution check (#1423)
    * Refac distribution code and add filesystem to unit tests
    
    * lint
    
    * fix pyspark error for dist check sql
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/core/tests/data_source/test_distribution_check.py
+++ soda/core/tests/data_source/test_distribution_check.py
@@ -1,30 +1,25 @@
-import os
 from textwrap import dedent
 
 import pytest
 from tests.helpers.common_test_tables import customers_dist_check_test_table
 from tests.helpers.data_source_fixture import DataSourceFixture
 from tests.helpers.fixtures import test_data_source
+from tests.helpers.mock_file_system import MockFileSystem
 
 
 @pytest.mark.skipif(
     test_data_source == "athena",
     reason="TODO: fix for athena.",
 )
-def test_distribution_check(data_source_fixture: DataSourceFixture, mock_file_system):
+def test_distribution_check(data_source_fixture: DataSourceFixture, mock_file_system: MockFileSystem):
     table_name = data_source_fixture.ensure_test_table(customers_dist_check_test_table)
+    table_name = data_source_fixture.data_source.default_casify_table_name(table_name)
 
-    user_home_dir = os.path.expanduser("~")
+    scan = data_source_fixture.create_test_scan()
+
+    user_home_dir = mock_file_system.user_home_dir()
 
     mock_file_system.files = {
-        f"{user_home_dir}/the_distribution_check_file.yml": dedent(
-            f"""
-                checks for {table_name}:
-                  - distribution_difference(size, my_happy_ml_model_distribution) >= 0.05:
-                      distribution reference file: ./customers_size_distribution_reference.yml
-                      method: ks
-            """
-        ).strip(),
         f"{user_home_dir}/customers_size_distribution_reference.yml": dedent(
             f"""
             dataset: {table_name}
@@ -37,10 +32,41 @@ def test_distribution_check(data_source_fixture: DataSourceFixture, mock_file_sy
         ).strip(),
     }
 
-    # TODO: do this logic at mock file system (its already added above to the mock file system)
-    ref_file = f"{user_home_dir}/customers_size_distribution_reference.yml"
-    with open(ref_file, "w") as f:
-        reference_table = f"""
+    scan.add_sodacl_yaml_str(
+        f"""
+        checks for {table_name}:
+            - distribution_difference(size, my_happy_ml_model_distribution) >= 0.05:
+                distribution reference file: {user_home_dir}/customers_size_distribution_reference.yml
+                method: ks
+    """
+    )
+
+    scan.enable_mock_soda_cloud()
+    scan.execute()
+
+
+@pytest.mark.parametrize(
+    "table, expectation",
+    [
+        pytest.param(
+            customers_dist_check_test_table, "SELECT \n  size \nFROM {schema_name}{table_name}\n LIMIT 1000000"
+        ),
+    ],
+)
+@pytest.mark.skipif(
+    test_data_source == "athena",
+    reason="TODO: fix for athena.",
+)
+def test_distribution_sql(data_source_fixture: DataSourceFixture, mock_file_system, table, expectation):
+    table_name = data_source_fixture.ensure_test_table(table)
+    table_name = data_source_fixture.data_source.default_casify_table_name(table_name)
+
+    scan = data_source_fixture.create_test_scan()
+    user_home_dir = mock_file_system.user_home_dir()
+
+    mock_file_system.files = {
+        f"{user_home_dir}/customers_size_distribution_reference.yml": dedent(
+            f"""
             dataset: {table_name}
             column: size
             distribution_type: continuous
@@ -48,61 +74,23 @@ def test_distribution_check(data_source_fixture: DataSourceFixture, mock_file_sy
                 bins: [1, 2, 3]
                 weights: [0.5, 0.2, 0.3]
         """
-        f.write(reference_table)
+        ).strip(),
+    }
 
-    scan = data_source_fixture.create_test_scan()
-    scan._configuration.file_system = mock_file_system
+    scan.add_sodacl_yaml_str(
+        f"""
+            checks for {table_name}:
+                - distribution_difference(size, my_happy_ml_model_distribution) >= 0.05:
+                    distribution reference file:  {user_home_dir}/customers_size_distribution_reference.yml
+                    method: ks
+        """
+    )
+
     scan.enable_mock_soda_cloud()
-    scan.add_sodacl_yaml_file(f"{user_home_dir}/the_distribution_check_file.yml")
     scan.execute()
-
-    # TODO: also remove this when removing the direct file system usage
-    os.remove(ref_file)
-
-
-# @pytest.mark.parametrize(
-#     "table, expectation",
-#     [
-#         pytest.param(customers_dist_check_test_table, "SELECT \n  size \nFROM {table_name}\n LIMIT 1000000"),
-#     ],
-# )
-# @pytest.mark.skipif(
-#     test_data_source == "athena",
-#     reason="TODO: fix for athena.",
-# )
-# def test_distribution_sql(data_source_fixture: DataSourceFixture, mock_file_system, table, expectation):
-#     table_name = data_source_fixture.ensure_test_table(table)
-#
-#     user_home_dir = os.path.expanduser("~")
-#
-#     mock_file_system.files = {
-#         f"{user_home_dir}/the_distribution_check_file.yml": dedent(
-#             f"""
-#                 checks for {table_name}:
-#                   - distribution_difference(size, my_happy_ml_model_distribution) >= 0.05:
-#                       distribution reference file: ./customers_size_distribution_reference.yml
-#                       method: ks
-#             """
-#         ).strip(),
-#     }
-#     # TODO: do this logic at mock file system
-#     ref_file = f"{user_home_dir}/customers_size_distribution_reference.yml"
-#     with open(ref_file, "w") as f:
-#         reference_table = f"""
-#             table: {table_name}
-#             column: size
-#             distribution_type: continuous
-#             distribution reference:
-#                 bins: [1, 2, 3]
-#                 weights: [0.5, 0.2, 0.3]
-#         """
-#         f.write(reference_table)
-#
-#     scan = data_source_fixture.create_test_scan()
-#     scan._configuration.file_system = mock_file_system
-#     scan.enable_mock_soda_cloud()
-#     scan.add_sodacl_yaml_file(f"{user_home_dir}/the_distribution_check_file.yml")
-#     scan.execute()
-#
-#     os.remove(ref_file)
-#     assert scan._checks[0].query.sql == expectation.format(table_name=table_name)
+    if test_data_source != "spark_df":
+        assert scan._checks[0].query.sql == expectation.format(
+            table_name=table_name, schema_name=data_source_fixture.schema_name + "."
+        )
+    else:
+        assert scan._checks[0].query.sql == expectation.format(table_name=table_name, schema_name="")
