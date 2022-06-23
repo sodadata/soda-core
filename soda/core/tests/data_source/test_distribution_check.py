f30742ae8c166dda6385c01a4704737890459d69
tituskx <tituskarelex@gmail.com>
2022-06-23 14:43:44 +0200

    change 'update' into 'update-dro' in error message in unit test

--- soda/core/tests/data_source/test_distribution_check.py
+++ soda/core/tests/data_source/test_distribution_check.py
@@ -121,7 +121,7 @@ def test_distribution_missing_bins_weights(data_source_fixture: DataSourceFixtur
     log_message = (
         'The DRO in your "/Users/johndoe/customers_size_distribution_reference.yml" distribution reference file does'
         ' not contain a "distribution_reference" key with weights and bins. Make sure that before running "soda scan" you'
-        ' create a DRO by running "soda update". For more information visit the docs:\nhttps://docs.soda.io/soda-cl/distribution.html#generate-a-distribution-reference-object-dro.'
+        ' create a DRO by running "soda update-dro". For more information visit the docs:\nhttps://docs.soda.io/soda-cl/distribution.html#generate-a-distribution-reference-object-dro.'
     )
 
     log = next(log for log in scan._logs.logs if isinstance(log.message, MissingBinsWeightsException))
