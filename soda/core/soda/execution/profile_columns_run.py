75c7f0d4f3f3ebb0c7893dc203aaa5c92cdbe5c5
Baturay Ofluoglu <baturayofluoglu@gmail.com>
2022-07-18 11:00:14 +0200

    fix: better logging messages for profiling and discover datasets (#1498)
    * better log messages for profiling and discover datasets
    * raise error if tables have quotes
    * more informative logs for sampling tables
    
    Co-authored-by: Bastien Boutonnet <bastien.b1@gmail.com>
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/core/soda/execution/profile_columns_run.py
+++ soda/core/soda/execution/profile_columns_run.py
@@ -52,20 +52,18 @@ class ProfileColumnsRun:
         include_tables = self._get_table_expression(self.profile_columns_cfg.include_columns)
         exclude_tables = self._get_table_expression(self.profile_columns_cfg.exclude_columns)
 
-        if include_tables or exclude_tables:
-            # row_counts is a dict that maps table names to row counts.
-            table_names: dict[str, int] = self.data_source.get_table_names(
-                include_tables=include_tables,
-                exclude_tables=exclude_tables,
-                query_name="profile-columns-get-table-names",
-            )
-        else:
-            table_names = []
+        # row_counts is a dict that maps table names to row counts.
+        table_names: dict[str, int] = self.data_source.get_table_names(
+            include_tables=include_tables,
+            exclude_tables=exclude_tables,
+            query_name="profile-columns-get-table-names",
+        )
 
         if len(table_names) < 1:
-            self.logs.warning(
+            self.logs.error(
                 f"No table matching your SodaCL inclusion list found on your {self.data_source.data_source_name} "
-                "data source. Profiling results may be incomplete or entirely skipped"
+                "data source. Profiling results may be incomplete or entirely skipped",
+                location=self.profile_columns_cfg.location,
             )
             return profile_columns_result
         parsed_included_tables_and_columns = self._build_column_expression_list(
@@ -74,8 +72,10 @@ class ProfileColumnsRun:
         parsed_excluded_tables_and_columns = self._build_column_expression_list(
             self.profile_columns_cfg.exclude_columns
         )
+
+        self.logs.info(f"Profiling columns for the following tables:")
         for table_name in table_names:
-            self.logs.debug(f"Profiling columns for {table_name}")
+            self.logs.info(f"  - {table_name}")
             measured_row_count = self.data_source.get_table_row_count(table_name)
             profile_columns_result_table = profile_columns_result.create_table(
                 table_name, self.data_source.data_source_name, measured_row_count
@@ -133,7 +133,10 @@ class ProfileColumnsRun:
                         )
 
             else:
-                self.logs.warning(f"No columns matching your SodaCL inclusion patterns were found on {table_name}.")
+                self.logs.error(
+                    f"No columns matching your SodaCL inclusion patterns were found on {table_name}.",
+                    location=self.profile_columns_cfg.location,
+                )
         return profile_columns_result
 
     def profile_numeric_column(
