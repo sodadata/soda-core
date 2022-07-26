7c52d70c58b5d92d4ad476b8bae10f09959848a4
Milan Lukac <m1n0@users.noreply.github.com>
2022-07-26 15:17:27 -0700

    Publish data source packages for python 3.7 (#1514)
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

--- soda/db2/setup.py
+++ soda/db2/setup.py
@@ -3,9 +3,9 @@ import sys
 
 from setuptools import find_namespace_packages, setup
 
-if sys.version_info < (3, 8):
-    print("Error: Soda SQL requires at least Python 3.8")
-    print("Error: Please upgrade your Python version to 3.8 or later")
+if sys.version_info < (3, 7):
+    print("Error: Soda SQL requires at least Python 3.7")
+    print("Error: Please upgrade your Python version to 3.7 or later")
     sys.exit(1)
 
 package_name = "soda-core-db2"
