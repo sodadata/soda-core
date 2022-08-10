a6dd9d1f41d0dd5da1adf4136227266c5055f113
Vijay Kiran <mail@vijaykiran.com>
2022-08-10 11:50:58 +0200

    Bump to 3.0.4

--- soda/redshift/setup.py
+++ soda/redshift/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-redshift"
-package_version = "3.0.3"
+package_version = "3.0.4"
 description = "Soda Core Redshift Package"
 
 requires = [f"soda-core=={package_version}", "boto3"]
