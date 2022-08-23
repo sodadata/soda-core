cb4185ed8e4522b4301a6402042c71c4afc60c9f
Scott Fought <92835652+ScottAtDisney@users.noreply.github.com>
2022-08-23 03:34:44 -0400

    Trino data source (#1553)
    Adds support for trino
    Co-authored-by: Vijay Kiran <mail@vijaykiran.com>

---
+++ soda/trino/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import sys
+
+from setuptools import find_namespace_packages, setup
+
+if sys.version_info < (3, 8):
+    print("Error: Soda Core requires at least Python 3.8")
+    print("Error: Please upgrade your Python version to 3.8 or later")
+    sys.exit(1)
+
+package_name = "soda-core-trino"
+package_version = "3.0.4"
+description = "Soda Core Trino Package"
+
+requires = [f"soda-core=={package_version}", "trino>=0.315.0"]
+
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
