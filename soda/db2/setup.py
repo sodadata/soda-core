671ebf5aa5f4f3334004b6ef1fd720a8b6e7d76b
Vijay Kiran <mail@vijaykiran.com>
2022-09-21 21:20:26 +0200

    Bump to 3.0.8

--- soda/db2/setup.py
+++ soda/db2/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-db2"
-package_version = "3.0.7"
+package_version = "3.0.8"
 # TODO Add proper description
 description = "Soda Core IBM DB2 Package"
 
