dea729193abb64e0df5f74ec5f34297a53009df3
Ceyhun Kerti <ceyhun.kerti@bluecolor.io>
2022-10-07 15:34:24 +0300

    add test for schema check with vars

--- soda/core/tests/data_source/test_vars.py
+++ soda/core/tests/data_source/test_vars.py
@@ -1,6 +1,7 @@
 from helpers.common_test_tables import customers_test_table
 from helpers.data_source_fixture import DataSourceFixture
 from helpers.utils import execute_scan_and_get_scan_result
+from soda.execution.check_outcome import CheckOutcome
 
 
 def test_vars_in_name(data_source_fixture: DataSourceFixture):
@@ -35,3 +36,25 @@ def test_vars_in_foreach_name(data_source_fixture: DataSourceFixture):
         """,
     )
     assert scan_result["checks"][0]["name"].lower() == f"Row count in {table_name} must be positive".lower()
+
+
+def test_vars_schema_check(data_source_fixture: DataSourceFixture):
+    table_name = data_source_fixture.ensure_test_table(customers_test_table)
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for ${{table}}:
+            - schema:
+                fail:
+                    when wrong column type:
+                        id: text
+        """
+    )
+    scan.add_variables({"table": table_name})
+
+    scan.execute()
+
+    check = scan._checks[0]
+
+    assert check.outcome == CheckOutcome.PASS
