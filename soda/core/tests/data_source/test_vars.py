8dc50051082f7daca3e82d3fec51cf0afb626d85
Vijay Kiran <mail@vijaykiran.com>
2022-10-18 13:50:10 +0200

    Remove schema var test

--- soda/core/tests/data_source/test_vars.py
+++ soda/core/tests/data_source/test_vars.py
@@ -38,23 +38,3 @@ def test_vars_in_foreach_name(data_source_fixture: DataSourceFixture):
     assert scan_result["checks"][0]["name"].lower() == f"Row count in {table_name} must be positive".lower()
 
 
-def test_vars_schema_check(data_source_fixture: DataSourceFixture):
-    table_name = data_source_fixture.ensure_test_table(customers_test_table)
-
-    scan = data_source_fixture.create_test_scan()
-    scan.add_sodacl_yaml_str(
-        f"""
-          checks for ${{table}}:
-            - schema:
-                fail:
-                    when wrong column type:
-                        id: text
-        """
-    )
-    scan.add_variables({"table": table_name})
-
-    scan.execute()
-
-    check = scan._checks[0]
-
-    assert check.outcome == CheckOutcome.PASS
