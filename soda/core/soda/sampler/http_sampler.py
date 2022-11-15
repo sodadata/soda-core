7eded3b231878357f7a025523c902db997999548
Vijay Kiran <mail@vijaykiran.com>
2022-11-15 22:05:01 +0100

    Fixes to sampler, add logging

--- soda/core/soda/sampler/http_sampler.py
+++ soda/core/soda/sampler/http_sampler.py
@@ -1,13 +1,9 @@
 from __future__ import annotations
 
-import logging
-
 import requests
 from soda.sampler.sample_context import SampleContext
 from soda.sampler.sample_ref import SampleRef
 from soda.sampler.sampler import Sampler
-
-logger = logging.getLogger(__name__)
 
 
 class HTTPSampler(Sampler):
@@ -18,8 +14,7 @@ class HTTPSampler(Sampler):
         self.message = message
 
     def store_sample(self, sample_context: SampleContext) -> SampleRef | None:
-
-        logger.info(f"Sending failed row samples to {self.url}")
+        self.logs.info(f"Sending failed row samples to {self.url}")
         sample_rows = sample_context.sample.get_rows()
         row_count = len(sample_rows)
         sample_schema = sample_context.sample.get_schema()
@@ -29,9 +24,9 @@ class HTTPSampler(Sampler):
         response = requests.post(self.url, json=result_dict)
 
         if response.status_code != 200:
-            logger.error(f"Unable to upload failed rows to {self.url} -  {response.text}")
+            self.logs.error(f"Unable to upload failed rows to {self.url} -  {response.text}")
         else:
-            logger.info(f"Uploaded {row_count} failed rows to {self.url}")
+            self.logs.info(f"Uploaded {row_count} failed rows to {self.url}")
 
         if sample_context.samples_limit is not None:
             stored_row_count = row_count if row_count < sample_context.samples_limit else sample_context.samples_limit
@@ -43,7 +38,7 @@ class HTTPSampler(Sampler):
             schema=sample_schema,
             total_row_count=row_count,
             stored_row_count=stored_row_count,
-            type="http",
+            type=SampleRef.TYPE_NOT_PERSISTED,
             link=self.link,
             message=self.message,
         )
