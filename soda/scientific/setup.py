49c03ea81a51258d3be71b85335aeb1af2ce5f21
Vijay Kiran <mail@vijaykiran.com>
2022-11-30 21:09:31 +0100

    Denodo support (experimental)

--- soda/scientific/setup.py
+++ soda/scientific/setup.py
@@ -4,7 +4,7 @@ import sys
 from setuptools import find_namespace_packages, setup
 
 if sys.version_info < (3, 8):
-    print("Error: Soda SQL requires at least Python 3.8")
+    print("Error: Soda Core requires at least Python 3.8")
     print("Error: Please upgrade your Python version to 3.8 or later")
     sys.exit(1)
 
