e19b05751ac7f2a19cdbf30a25afad9605b8572e
Vijay Kiran <mail@vijaykiran.com>
2022-12-27 17:21:35 +0100

    [DREMIO] fix profiling issues (#1746)

--- soda/core/soda/execution/data_source.py
+++ soda/core/soda/execution/data_source.py
@@ -669,7 +669,7 @@ class DataSource:
                         {mins_cte},
                         {maxs_cte},
                         {frequent_values_cte},
-                        result AS (
+                        results AS (
                             SELECT * FROM mins
                             {union}
                             SELECT * FROM maxs
@@ -677,7 +677,7 @@ class DataSource:
                             SELECT * FROM frequent_values
                         )
                     SELECT *
-                    FROM result
+                    FROM results
                     ORDER BY metric_ ASC, index_ ASC
                 """
             )
