4b7889466e43af0022db31be959c664c75580405
tituskx <tituskarelex@gmail.com>
2023-01-10 14:49:00 +0100

    change order of methods in duckdb

--- soda/duckdb/soda/data_sources/duckdb_data_source.py
+++ soda/duckdb/soda/data_sources/duckdb_data_source.py
@@ -127,12 +127,12 @@ class DuckDBDataSource(DataSource):
         return ""
 
     @staticmethod
-    def column_metadata_datatype_name() -> str:
-        return "lower(data_type) as data_type"
-
-    @staticmethod
     def column_metadata_columns() -> list:
         return ["column_name", "lower(data_type) as data_type", "is_nullable"]
+        
+    @staticmethod
+    def column_metadata_datatype_name() -> str:
+        return "lower(data_type) as data_type"
 
     def get_metric_sql_aggregation_expression(self, metric_name: str, metric_args: Optional[List[object]], expr: str):
         # https://duckdb.org/docs/sql/aggregates
