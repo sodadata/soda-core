de3cadfd9ea28e08517382e35c1341a4df06d8d8
tituskx <tituskarelex@gmail.com>
2023-01-10 12:09:52 +0100

    remove profiling in names from spark

--- soda/spark/soda/data_sources/spark_data_source.py
+++ soda/spark/soda/data_sources/spark_data_source.py
@@ -215,7 +215,7 @@ class SparkSQLBase(DataSource):
         return ""
 
     @staticmethod
-    def pattern_matches_profiling(spark_object_name: str, spark_object_name_pattern: str | None) -> bool:
+    def pattern_matches(spark_object_name: str, spark_object_name_pattern: str | None) -> bool:
         if spark_object_name_pattern is None: 
             return True 
         else:
@@ -223,7 +223,7 @@ class SparkSQLBase(DataSource):
             is_match = re.fullmatch(pattern_regex, spark_object_name.lower())
             return bool(is_match)
 
-    def get_included_table_names_profiling(
+    def get_included_table_names(
         self, query_name: str, include_patterns: list[dict[str, str]], exclude_patterns: list[dict[str, str]], table_names_only: bool = False
     ) -> list[str]:
         query = Query(
@@ -238,11 +238,11 @@ class SparkSQLBase(DataSource):
             table_name
             for table_name in table_names
             if any(
-                self.pattern_matches_profiling(table_name, include_pattern["table_name_pattern"])
+                self.pattern_matches(table_name, include_pattern["table_name_pattern"])
                 for include_pattern in include_patterns
             )
             and not any(
-                self.pattern_matches_profiling(table_name, exclude_pattern["table_name_pattern"])
+                self.pattern_matches(table_name, exclude_pattern["table_name_pattern"])
                 and (exclude_pattern.get("column_name_pattern") == "%" or table_names_only)
                 for exclude_pattern in exclude_patterns
             )
@@ -250,13 +250,13 @@ class SparkSQLBase(DataSource):
 
         return included_table_names
 
-    def column_table_pattern_match_profiling(
+    def column_table_pattern_match(
         self, table_name: str, column_name: str, profiling_patterns: list[dict[str, str]]
     ) -> bool:
         column_table_name_pattern_match = any(
             (
-                self.pattern_matches_profiling(table_name, pattern["table_name_pattern"])
-                and self.pattern_matches_profiling(column_name, pattern.get("column_name_pattern"))
+                self.pattern_matches(table_name, pattern["table_name_pattern"])
+                and self.pattern_matches(column_name, pattern.get("column_name_pattern"))
             )
             for pattern in profiling_patterns
         )
@@ -269,7 +269,7 @@ class SparkSQLBase(DataSource):
         exclude_patterns: list[dict[str, str]] | list[str] | None = None,
         table_names_only: bool = False 
     ) -> dict[str, str] | None:
-        included_table_names: list[str] = self.get_included_table_names_profiling(query_name, include_patterns, exclude_patterns, table_names_only=table_names_only)
+        included_table_names: list[str] = self.get_included_table_names(query_name, include_patterns, exclude_patterns, table_names_only=table_names_only)
         if table_names_only:
             return included_table_names
         tables_and_columns_metadata = defaultdict(dict)
@@ -288,10 +288,10 @@ class SparkSQLBase(DataSource):
 
             if columns_metadata and len(columns_metadata) > 0:
                 for column_name, column_datatype, _ in columns_metadata:
-                    column_name_included = self.column_table_pattern_match_profiling(
+                    column_name_included = self.column_table_pattern_match(
                         table_name, column_name, include_patterns
                     )
-                    column_name_excluded = self.column_table_pattern_match_profiling(
+                    column_name_excluded = self.column_table_pattern_match(
                         table_name, column_name, exclude_patterns
                     )
                     if column_name_included and not column_name_excluded:
