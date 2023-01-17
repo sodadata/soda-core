c2b2fa899624464ed6ced1892431e49acfe3ad15
baturayo <baturayofluoglu@gmail.com>
2023-01-17 23:53:38 +0100

    fix unit tests for dask

--- soda/dask/tests/dask_data_source_fixture.py
+++ soda/dask/tests/dask_data_source_fixture.py
@@ -4,7 +4,6 @@ import logging
 
 import dask.dataframe as dd
 import pandas as pd
-from dask_sql import Context
 from helpers.data_source_fixture import DataSourceFixture
 from helpers.test_table import TestTable
 from soda.scan import Scan
@@ -15,14 +14,13 @@ logger = logging.getLogger(__name__)
 class DaskDataSourceFixture(DataSourceFixture):
     def __init__(self, test_data_source: str):
         super().__init__(test_data_source)
-        self.context = Context()
 
     def _build_configuration_dict(self, schema_name: str | None = None) -> dict:
         return {"data_source dask": {"type": "dask"}, "schema": schema_name}
 
     def _test_session_starts(self) -> None:
         scan = Scan()
-        scan.add_dask_context(dask_context=self.context, data_source_name=self.data_source_name)
+        self.context = scan._get_or_create_dask_context(required_soda_module="soda-core-dask")
         self.data_source = scan._data_source_manager.get_data_source(self.data_source_name)
         scan._get_or_create_data_source_scan(self.data_source_name)
 
