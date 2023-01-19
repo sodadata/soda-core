0c93f9907d4be7609300eda49967bc76c893f80a
Milan Lukac <milan@lukac.online>
2023-01-19 17:48:44 +0100

    Bump to 3.0.21

--- soda/sqlserver/setup.py
+++ soda/sqlserver/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-sqlserver"
-package_version = "3.0.20"
+package_version = "3.0.21"
 description = "Soda Core SQL Server Package"
 
 requires = [
