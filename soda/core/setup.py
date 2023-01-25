a52b8b0f96d91c4639179fa8d58a8c7ab0558334
Vijay Kiran <mail@vijaykiran.com>
2023-01-25 22:11:10 +0530

    Bump to 3.0.22

--- soda/core/setup.py
+++ soda/core/setup.py
@@ -10,7 +10,7 @@ if sys.version_info < (3, 7):
 
 package_name = "soda-core"
 # Managed by tbump - do not change manually
-package_version = "3.0.21"
+package_version = "3.0.22"
 description = "Soda Core"
 
 requires = [
