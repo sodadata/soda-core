361aa9ffc3665cdecdcf8b9b7f038116e269848c
Milan Lukac <milan@lukac.online>
2023-01-26 09:59:18 +0100

    Failed rows diagnostics block

--- soda/core/soda/execution/check/metric_check.py
+++ soda/core/soda/execution/check/metric_check.py
@@ -132,12 +132,7 @@ class MetricCheck(Check):
         if metric_check_cfg.warn_threshold_cfg is not None:
             cloud_diagnostics["warn"] = metric_check_cfg.warn_threshold_cfg.to_soda_cloud_diagnostics_json()
         if self.failed_rows_sample_ref:
-            file_block = {
-                "type": "file",
-                "title": "Failed Rows Sample",
-                "file": self.failed_rows_sample_ref.get_cloud_diagnostics_dict(),
-            }
-            cloud_diagnostics["blocks"] = [file_block]
+            cloud_diagnostics["blocks"] = [self.failed_rows_sample_ref.get_cloud_diagnostics_block()]
         return cloud_diagnostics
 
     def get_log_diagnostic_dict(self) -> dict:
