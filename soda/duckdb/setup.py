bdc4ad0824906087a1c1ded3667ba8c98d7c102f
baturayo <baturayofluoglu@gmail.com>
2023-02-14 10:31:41 +0100

    pin duckdb version

--- soda/duckdb/setup.py
+++ soda/duckdb/setup.py
@@ -12,7 +12,7 @@ package_name = "soda-core-duckdb"
 package_version = "3.0.22"
 description = "Soda Core Duckdb Package"
 
-requires = [f"soda-core=={package_version}", "duckdb"]
+requires = [f"soda-core=={package_version}", "duckdb==0.6.1"]
 
 setup(
     name=package_name,
