c0f92e23f7b91777a0075c12efea73f649c83707
Vijay Kiran <mail@vijaykiran.com>
2023-03-21 22:44:32 +0100

    Bump to 3.0.30

--- soda/oracle/setup.py
+++ soda/oracle/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-oracle"
-package_version = "3.0.29"
+package_version = "3.0.30"
 # TODO Add proper description
 description = "Soda Core Oracle Package"
 
