0dd135ef728b2cf50699defd602e25df9fc99e53
tituskx <tituskarelex@gmail.com>
2023-03-23 13:32:43 +0100

    create new unit test that checks that weights and bins are created correctly

--- soda/core/tests/cli/test_cli_update_distribution_file.py
+++ soda/core/tests/cli/test_cli_update_distribution_file.py
@@ -1,8 +1,8 @@
 from textwrap import dedent
-
+from ruamel.yaml import YAML
 import pytest
 from cli.run_cli import run_cli
-from helpers.common_test_tables import customers_test_table
+from helpers.common_test_tables import customers_test_table, dro_categorical_test_table
 from helpers.data_source_fixture import DataSourceFixture
 from helpers.fixtures import test_data_source
 from helpers.mock_file_system import MockFileSystem
@@ -39,3 +39,41 @@ def test_cli_update_distribution_file(data_source_fixture: DataSourceFixture, mo
         ]
     )
     assert ("weights" and "bins") in mock_file_system.files[f"{user_home_dir}/customers_distribution_reference.yml"]
+
+@pytest.mark.skipif(
+    test_data_source in ["spark_df", "spark"],
+    reason="Run for postgres only as nothing data source specific is tested.",
+)
+def test_cli_update_distribution_file_categorical(data_source_fixture: DataSourceFixture, mock_file_system: MockFileSystem):
+    table_name = data_source_fixture.ensure_test_table(dro_categorical_test_table)
+
+    user_home_dir = mock_file_system.user_home_dir()
+
+    mock_file_system.files = {
+        f"{user_home_dir}/configuration.yml": data_source_fixture.create_test_configuration_yaml_str(),
+        f"{user_home_dir}/customers_distribution_reference.yml": dedent(
+            f"""
+                table: {table_name}
+                column: categorical_value
+                distribution_type: categorical
+            """
+        ),
+    }
+
+    run_cli(
+        [
+            "update-dro",
+            "-c",
+            "configuration.yml",
+            "-d",
+            data_source_fixture.data_source.data_source_name,
+            f"{user_home_dir}/customers_distribution_reference.yml",
+        ]
+    )
+    parsed_dro: dict = YAML().load(mock_file_system.files[f"{user_home_dir}/customers_distribution_reference.yml"])
+    weights = parsed_dro['distribution_reference']['weights']
+    bins = parsed_dro['distribution_reference']['bins']
+    bins_and_weights_sorted = sorted(zip(bins, weights))
+    assert bins_and_weights_sorted == [(0, 0.13636363636363635), (1, 0.13636363636363635), (2, 0.2727272727272727), (3, 0.18181818181818182), (6, 0.2727272727272727)]
+
+    
