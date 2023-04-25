eda57acbfde4a876237ef1d4f696ee56ef4657ce
Vijay Kiran <mail@vijaykiran.com>
2023-04-25 15:51:54 +0200

    Bump to 3.0.33

--- soda/spark/setup.py
+++ soda/spark/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-spark"
-package_version = "3.0.32"
+package_version = "3.0.33"
 description = "Soda Core Spark Package"
 
 requires = [f"soda-core=={package_version}"]
