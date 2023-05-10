c4340b66db99b54b4894d71c30978e3d57c50f8c
Milan Lukac <milan@lukac.online>
2023-05-09 22:53:17 -0700

    Bump to 3.0.34

--- soda/athena/setup.py
+++ soda/athena/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-athena"
-package_version = "3.0.33"
+package_version = "3.0.34"
 description = "Soda Core Athena Package"
 
 requires = [
