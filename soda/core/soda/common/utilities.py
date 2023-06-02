642d45703fa319a6f73696d063afc351f957dd45
Milan Lukac <milan@lukac.online>
2023-06-01 20:56:12 -0700

    Update Library detector import path

--- soda/core/soda/common/utilities.py
+++ soda/core/soda/common/utilities.py
@@ -1,6 +1,6 @@
 def is_soda_library_available() -> bool:
     try:
-        from soda.execution.check.cloud_check import CloudCheckMixin
+        from soda_library.execution.check.cloud_check import CloudCheckMixin
 
         return True
     except ModuleNotFoundError:
