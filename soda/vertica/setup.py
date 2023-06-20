a7574719fb6270d5da2c3b248458bb586e5d380e
Vijay Kiran <mail@vijaykiran.com>
2023-06-20 14:05:41 +0200

    Bump to 3.0.41

--- soda/vertica/setup.py
+++ soda/vertica/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 7):
     sys.exit(1)
 
 package_name = "soda-core-vertica"
-package_version = "3.0.40"
+package_version = "3.0.41"
 description = "Soda Core Vertica Package"
 
 requires = [f"soda-core=={package_version}", "vertica-python>=1.0.3, <2.0"]
