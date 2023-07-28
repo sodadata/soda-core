78de6af4236b0f04a82e738293aada56e2137f98
Vijay Kiran <mail@vijaykiran.com>
2023-07-28 13:01:31 +0200

    Bump to 3.0.47

--- soda/duckdb/setup.py
+++ soda/duckdb/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-duckdb"
-package_version = "3.0.46"
+package_version = "3.0.47"
 description = "Soda Core Duckdb Package"
 
 requires = [f"soda-core=={package_version}", "duckdb<=0.8"]
