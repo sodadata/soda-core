eae908dc56e5f598107185b1ef33e87414ef515f
Paolo Leonard <paololeonard@skynet.be>
2023-10-01 14:05:52 +0200

    Added Json tests

--- soda/duckdb/tests/test_duckdb.py
+++ soda/duckdb/tests/test_duckdb.py
@@ -56,3 +56,36 @@ def test_df_from_csv(data_source_fixture: DataSourceFixture, tmp_path: Path):
     scan.execute(allow_warnings_only=True)
     scan.assert_all_checks_pass()
     scan.assert_no_error_logs()
+    
+def test_df_from_json(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    import logging
+    import pandas as pd
+    from soda.common.logs import Logs
+    
+    json_folder = tmp_path / "json"
+    json_folder.mkdir()
+    json_path = csv_folder / "dataset.csv"
+    
+    test_df = pd.DataFrame.from_dict({"i": [1, 2, 3, 4], "j": ["one", "two", "three", "four"]})
+    test_df.to_csv(json_path)
+    
+    data_source_properties = {
+      "path": json_path,
+    }
+
+    data_source = DuckDBDataSource(logs=Logs(logging.getLogger("test_df_from_csv")), 
+                                   data_source_name="dataset",
+                                   data_source_properties=data_source_properties)
+    
+    scan = data_source.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for dataset:
+            - row_count = 4
+            - missing_count(i) = 0
+            - missing_count(j) = 0
+        """
+    )
+    scan.execute(allow_warnings_only=True)
+    scan.assert_all_checks_pass()
+    scan.assert_no_error_logs()
