05065cfb327fa2c18fc1515e6e51eb2253bbeb5b
tombaeyens <mail@tombaeyens.be>
2023-11-08 09:47:45 +0100

    added setup.py

---
+++ soda/contracts/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import find_namespace_packages, setup
+
+package_name = "soda-core-contracts"
+package_version = "3.0.51"
+description = "Soda Core Contracts Package"
+
+requires = [
+    f"soda-core=={package_version}",
+]
+# TODO Fix the params
+setup(
+    name=package_name,
+    version=package_version,
+    install_requires=requires,
+    packages=find_namespace_packages(include=["soda*"]),
+)
