b7103e1faefe9bdd93a389cb99d699fc9aa16e42
Milan Lukac <milan@lukac.online>
2023-12-15 13:28:05 -0800

    Bump to 3.1.2

--- soda/bigquery/setup.py
+++ soda/bigquery/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-bigquery"
-package_version = "3.1.1"
+package_version = "3.1.2"
 description = "Soda Core Bigquery Package"
 
 requires = [
--- soda/dremio/setup.py
+++ soda/dremio/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-dremio"
-package_version = "3.1.1"
+package_version = "3.1.2"
 description = "Soda Core Dremio Package"
 
 requires = [f"soda-core=={package_version}", "pyodbc", "pyarrow"]
