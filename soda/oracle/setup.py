64bc3383a5d9ae8ef2b579f9f25c870f8fc8c033
Milan Lukac <milan@lukac.online>
2024-01-24 11:55:14 -0800

    Bump to 3.1.4

--- soda/oracle/setup.py
+++ soda/oracle/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-oracle"
-package_version = "3.1.3"
+package_version = "3.1.4"
 # TODO Add proper description
 description = "Soda Core Oracle Package"
 
