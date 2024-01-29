736543adfa3434627d60b755e3f8d1a8afe300fc
Milan Lukac <milan@lukac.online>
2024-01-29 14:48:00 -0800

    Unpin dask
    Fixes #1989

--- soda/dask/setup.py
+++ soda/dask/setup.py
@@ -6,7 +6,7 @@ package_name = "soda-core-pandas-dask"
 package_version = "3.2.1"
 description = "Soda Core Dask Package"
 
-requires = [f"soda-core=={package_version}", "dask>=2022.10.0", "dask-sql>=2022.12.0,<2023.6.0"]
+requires = [f"soda-core=={package_version}", "dask>=2022.10.0", "dask-sql>=2022.12.0"]
 
 setup(
     name=package_name,
