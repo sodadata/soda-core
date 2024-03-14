72e24f539738044de095e1f6144ec70dc2f11c88
Milan Lukac <milan@lukac.online>
2024-03-14 13:52:54 -0700

    Fix fixtures imports

--- soda/contracts/tests/conftest.py
+++ soda/contracts/tests/conftest.py
@@ -1,2 +1,3 @@
-from contracts.helpers.contract_fixtures import *  # NOQA
+# Import generic fixtures first as Telemetry needs to be set up first.
 from helpers.fixtures import *  # NOQA
+from contracts.helpers.contract_fixtures import *  # NOQA
