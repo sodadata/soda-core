a1a200862e79af4f01847241133d88a1260568b8
Bastien Boutonnet <bastien.b1@gmail.com>
2024-03-19 17:29:15 +0100

    feat: improved wording and tooltip formatting in simulator (#2038)

--- soda/scientific/soda/scientific/anomaly_detection_v2/simulate/visualisation.py
+++ soda/scientific/soda/scientific/anomaly_detection_v2/simulate/visualisation.py
@@ -22,8 +22,8 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
     fig = go.Figure()
 
     # Add y and y^ traces
-    fig.add_trace(go.Scatter(x=df.ds, y=df.value, mode="lines", name="y"))
-    fig.add_trace(go.Scatter(x=df.ds, y=df.yhat, mode="lines", name="y^", line=dict(dash="dash")))
+    fig.add_trace(go.Scatter(x=df.ds, y=df.value, mode="lines", name="Measured Value"))
+    fig.add_trace(go.Scatter(x=df.ds, y=df.yhat, mode="lines", name="Predicted Value", line=dict(dash="dash")))
 
     # Fill the area between bounds
     fig.add_trace(
@@ -33,7 +33,7 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
             fill="toself",
             fillcolor="rgba(255,255,0,0.2)",  # light yellow for fail
             line=dict(color="rgba(255,255,0,0.2)"),
-            name="warning range",
+            name="Anomaly Warning Range",
         )
     )
 
@@ -44,7 +44,7 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
             fill="toself",
             fillcolor="rgba(0,255,0,0.2)",  # light green for pass
             line=dict(color="rgba(255,255,255,0)"),
-            name="pass range",
+            name="No Anomaly Range",
         )
     )
 
@@ -55,16 +55,22 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
         | ((df.value < df.warn_lower_bound) & (df.value >= df.fail_lower_bound))
     ]
     pass_points = df[(df.value <= df.warn_upper_bound) & (df.value >= df.warn_lower_bound)]
-    custom_data = ["yhat", "warn_lower_bound", "fail_lower_bound", "warn_upper_bound", "fail_upper_bound"]
+    custom_data = [
+        "yhat",
+        "warn_lower_bound",
+        "fail_lower_bound",
+        "warn_upper_bound",
+        "fail_upper_bound",
+    ]
     # Hover over template
     hover_over_template = (
-        "Scan Time: %{x}<br>"
-        + "Measurement: %{y}<br>"  # Display X value
-        + "y^: %{customdata[0]:.4f}<br>"  # Display Y value
-        + "Warn Upper Bound: %{customdata[3]:.4f}<br>"  # Display y^ value
-        + "Fail Upper Bound: %{customdata[4]:.4f}<br>"  # Display warn_upper_bound value
-        + "Warn Lower Bound: %{customdata[1]:.4f}<br>"  # Display fail_upper_bound value
-        + "Fail Lower Bound: %{customdata[2]:.4f}<br>"  # Display warn_lower_bound value
+        "Scan Time: %{x|%Y-%m-%d %H:%M:%S}<br>"
+        + "Warning Upper Bound: %{customdata[3]:,.4f}<br>"  # Display y^ value
+        + "Anomaly Upper Bound: %{customdata[4]:,.4f}<br>"  # Display warn_upper_bound value
+        + "<b>Actual Value: %{y}</b><br>"  # Display X value
+        + "Predicted Value: %{customdata[0]:,.4f}<br>"  # Display Y value
+        + "Warning Lower Bound: %{customdata[1]:,.4f}<br>"  # Display fail_upper_bound value
+        + "Anomaly Lower Bound: %{customdata[2]:,.4f}<br>"  # Display warn_lower_bound value
         + "<extra></extra>"  # Display fail_lower_bound value
     )
     fig.add_trace(
@@ -72,7 +78,7 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
             x=fail_points.ds,
             y=fail_points.value,
             mode="markers",
-            name="fail",
+            name="Critical Anomaly",
             marker=dict(color="red", size=10),
             hovertemplate=hover_over_template,
             customdata=fail_points[custom_data],
@@ -83,7 +89,7 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
             x=warn_points.ds,
             y=warn_points.value,
             mode="markers",
-            name="warn",
+            name="Warning Anomaly",
             marker=dict(color="yellow", size=10),
             hovertemplate=hover_over_template,
             customdata=warn_points[custom_data],
@@ -94,7 +100,7 @@ def visualize_results(results: AnomalyDetectionResults) -> go.Figure:
             x=pass_points.ds,
             y=pass_points.value,
             mode="markers",
-            name="pass",
+            name="No Anomaly",
             marker=dict(color="green", size=3),
             hovertemplate=hover_over_template,
             customdata=pass_points[custom_data],
