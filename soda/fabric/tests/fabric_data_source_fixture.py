7ec404eef515b72c333716292d59a8a5a593ce85
JasperH <jasper.heldenbergh@student.ucll.be>
2024-04-03 13:36:10 +0200

    More tests

--- soda/fabric/tests/fabric_data_source_fixture.py
+++ soda/fabric/tests/fabric_data_source_fixture.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import os
+from helpers.data_source_fixture import TestTable
 
 from helpers.data_source_fixture import DataSourceFixture
 
@@ -27,16 +28,174 @@ class FabricDataSourceFixture(DataSourceFixture):
             }
         }
 
-    def _create_schema_if_not_exists_sql(self) -> str:
-        return (
-            f"IF NOT EXISTS (SELECT * FROM sys.schemas WHERE name = '{self.schema_name}') "
-            f"BEGIN EXEC('CREATE SCHEMA {self.schema_name};') "
-            f"END"
-        )
-
-    def _drop_schema_if_exists_sql(self):
+    def _drop_schema_if_exists_sql(self) -> str:
         return (
             f"IF EXISTS (SELECT * FROM sys.schemas WHERE name = '{self.schema_name}') "
             f"BEGIN EXEC('DROP SCHEMA {self.schema_name};') "
             f"END"
         )
+
+    def _drop_all_tables_in_schema_sql(self):
+        return """
+            DECLARE @sql NVARCHAR(MAX) = N'';
+
+            SELECT @sql += 'DROP TABLE [' + TABLE_SCHEMA + '].[' + TABLE_NAME + '];' + CHAR(13)
+            FROM INFORMATION_SCHEMA.TABLES
+            WHERE TABLE_SCHEMA = @schemaName
+            AND TABLE_TYPE = 'BASE TABLE';
+
+            EXEC sp_executesql @sql, N'@schemaName NVARCHAR(MAX)', @schemaName = %s;
+            """ % self.schema_name
+
+    def _drop_all_tables_in_schema(self):
+        drop_all_tables_in_schema_sql = self._drop_all_tables_in_schema_sql()
+        self._update(drop_all_tables_in_schema_sql, self.schema_data_source.connection)
+
+    def _get_dataset_id(self):
+        return f"{self.schema_data_source.project_id}.{self.schema_name}"
+
+    def _create_schema_if_not_exists_sql(self):
+        return f"""
+        IF NOT EXISTS ( SELECT  *
+                        FROM    sys.schemas
+                        WHERE   name = N'{self.schema_name}' )
+        EXEC('CREATE SCHEMA [{self.schema_name}]');
+        """
+
+    def _drop_schema_if_exists(self):
+        return f"""
+        /* Drop all non-system stored procs */
+        DECLARE @name VARCHAR(128)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'P' AND category = 0 ORDER BY [name])
+
+        WHILE @name is not null
+        BEGIN
+            SELECT @SQL = 'DROP PROCEDURE [{self.schema_name}].[' + RTRIM(@name) +']'
+            EXEC (@SQL)
+            PRINT 'Dropped Procedure: ' + @name
+            SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'P' AND category = 0 AND [name] > @name ORDER BY [name])
+        END
+        GO
+
+        /* Drop all views */
+        DECLARE @name VARCHAR(128)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'V' AND category = 0 ORDER BY [name])
+
+        WHILE @name IS NOT NULL
+        BEGIN
+            SELECT @SQL = 'DROP VIEW [{self.schema_name}].[' + RTRIM(@name) +']'
+            EXEC (@SQL)
+            PRINT 'Dropped View: ' + @name
+            SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'V' AND category = 0 AND [name] > @name ORDER BY [name])
+        END
+        GO
+
+        /* Drop all functions */
+        DECLARE @name VARCHAR(128)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] IN (N'FN', N'IF', N'TF', N'FS', N'FT') AND category = 0 ORDER BY [name])
+
+        WHILE @name IS NOT NULL
+        BEGIN
+            SELECT @SQL = 'DROP FUNCTION [{self.schema_name}].[' + RTRIM(@name) +']'
+            EXEC (@SQL)
+            PRINT 'Dropped Function: ' + @name
+            SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] IN (N'FN', N'IF', N'TF', N'FS', N'FT') AND category = 0 AND [name] > @name ORDER BY [name])
+        END
+        GO
+
+        /* Drop all Foreign Key constraints */
+        DECLARE @name VARCHAR(128)
+        DECLARE @constraint VARCHAR(254)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME)
+
+        WHILE @name is not null
+        BEGIN
+            SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
+            WHILE @constraint IS NOT NULL
+            BEGIN
+                SELECT @SQL = 'ALTER TABLE [{self.schema_name}].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint) +']'
+                EXEC (@SQL)
+                PRINT 'Dropped FK Constraint: ' + @constraint + ' on ' + @name
+                SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONSTRAINT_NAME <> @constraint AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
+            END
+        SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'FOREIGN KEY' ORDER BY TABLE_NAME)
+        END
+        GO
+
+        /* Drop all Primary Key constraints */
+        DECLARE @name VARCHAR(128)
+        DECLARE @constraint VARCHAR(254)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME)
+
+        WHILE @name IS NOT NULL
+        BEGIN
+            SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
+            WHILE @constraint is not null
+            BEGIN
+                SELECT @SQL = 'ALTER TABLE [{self.schema_name}].[' + RTRIM(@name) +'] DROP CONSTRAINT [' + RTRIM(@constraint)+']'
+                EXEC (@SQL)
+                PRINT 'Dropped PK Constraint: ' + @constraint + ' on ' + @name
+                SELECT @constraint = (SELECT TOP 1 CONSTRAINT_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' AND CONSTRAINT_NAME <> @constraint AND TABLE_NAME = @name ORDER BY CONSTRAINT_NAME)
+            END
+        SELECT @name = (SELECT TOP 1 TABLE_NAME FROM INFORMATION_SCHEMA.TABLE_CONSTRAINTS WHERE constraint_catalog=DB_NAME() AND CONSTRAINT_TYPE = 'PRIMARY KEY' ORDER BY TABLE_NAME)
+        END
+        GO
+
+        /* Drop all tables */
+        DECLARE @name VARCHAR(128)
+        DECLARE @SQL VARCHAR(254)
+
+        SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'U' AND category = 0 ORDER BY [name])
+
+        WHILE @name IS NOT NULL
+        BEGIN
+            SELECT @SQL = 'DROP TABLE [{self.schema_name}].[' + RTRIM(@name) +']'
+            EXEC (@SQL)
+            PRINT 'Dropped Table: ' + @name
+            SELECT @name = (SELECT TOP 1 [name] FROM sysobjects WHERE [type] = 'U' AND category = 0 AND [name] > @name ORDER BY [name])
+        END
+        GO
+        """
+
+    def _drop_test_table_sql(self, table_name: str) -> str:
+        return (
+            f"DROP TABLE IF EXISTS {self.schema_name}.{table_name}"
+        )
+
+    def drop_test_table(self, table_name: str) -> None:
+        drop_test_table_sql = self._drop_test_table_sql(table_name)
+        self._update(drop_test_table_sql, self.schema_data_source.connection)
+
+    def ensure_test_table(self, test_table: TestTable) -> str:
+        self._drop_schema_if_exists()
+        existing_test_table_names = self._get_existing_test_table_names()
+        existing_test_table_names_lower = [table_name.lower() for table_name in existing_test_table_names]
+        if test_table.unique_table_name.lower() not in existing_test_table_names_lower:
+            obsolete_table_names = [
+                existing_test_table
+                for existing_test_table in existing_test_table_names
+                if existing_test_table.lower().startswith(f"sodatest_{test_table.name.lower()}_")
+            ]
+            if obsolete_table_names:
+                for obsolete_table_name in obsolete_table_names:
+                    self._drop_test_table(obsolete_table_name)
+            self._create_and_insert_test_table(test_table)
+            self.data_source.commit()
+
+            # Run table analyze so that internal data source statistics are refreshed before running any tests.
+            self.data_source.analyze_table(test_table.unique_table_name)
+        return test_table.unique_view_name if test_table.create_view else test_table.unique_table_name
+
+    def _create_view_from_table_sql(self, test_table: TestTable):
+        # @TOOD
+        pass
--- soda/fabric/tests/test_fabric.py
+++ soda/fabric/tests/test_fabric.py
@@ -1,50 +1,244 @@
 from pathlib import Path
-
-import pandas as pd
-import pyodbc
 from helpers.data_source_fixture import DataSourceFixture
+from helpers.data_source_fixture import TestTable
+
+import logging
 
 
-def test_connectivity(data_source_fixture: DataSourceFixture):
-    print("Starting SQL Server connection test...")
+logger = logging.getLogger(__name__)
 
-    conn_str = (
-        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
-        f"SERVER=localhost,1433;"
-        f"DATABASE=master;"
-        f"UID=sa;"
-        f"PWD=YourStrong!Passw0rd;"
-        f"Encrypt=No"
+
+def test_integer_types(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="NUMERIC_TYPES",
+            columns=[
+                ("c01", "smallint"),
+                ("c02", "bigint"),
+                ("c03", "integer"),
+                ("c04", "bigint"),
+                ("c05", "decimal"),
+                ("c06", "numeric"),
+                ("c07", "real"),
+                ("c08", "double precision"),
+            ],
+            values=[
+                (1, 1, 1, 1, 1, 1, 1, 1),
+                (None, None, None, None, None, None, None, None)
+            ],
+        )
     )
 
-    try:
-        with pyodbc.connect(conn_str) as conn:
-            print("Connected to SQL Server successfully.")
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+            - missing_count(c01) = 1
+            - missing_count(c02) = 1
+            - missing_count(c03) = 1
+            - missing_count(c04) = 1
+            - missing_count(c05) = 1
+            - missing_count(c06) = 1
+            - missing_count(c07) = 1
+            - missing_count(c08) = 1
+        """
+    )
+    scan.execute()
 
-            query = "SELECT 1 AS test_col"
-            df = pd.read_sql(query, conn)
-            print("Query executed and loaded into DataFrame successfully.")
-            print(df)
-
-    except Exception as e:
-        print(f"Error connecting to SQL Server: {e}")
-
-
-def test_pandas_df(data_source_fixture: DataSourceFixture, tmp_path: Path):
-    print("CSV test")
-    # TODO
-
-
-def test_df_from_csv(data_source_fixture: DataSourceFixture, tmp_path: Path):
-    print("CSV test")
-    # TODO
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
 
 
-def test_df_from_json(data_source_fixture: DataSourceFixture, tmp_path: Path):
-    print("Json test")
-    # TODO
+def test_string_types(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="STRING_TYPES",
+            columns=[
+                ("c01", "varchar"),
+                ("c02", "char"),
+                ("c03", "text"),
+            ],
+            values=[
+                ("a", "a", "a"),
+                (None, None, None)
+            ],
+        )
+    )
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+            - missing_count(c01) = 1
+            - missing_count(c02) = 1
+            - missing_count(c03) = 1
+        """
+    )
+    scan.execute()
+
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
 
 
-def test_df_from_parquet(data_source_fixture: DataSourceFixture, tmp_path: Path):
-    print("Parquet test")
-    # TODO
+def test_phone_numbers(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="PHONE_NUMBER_CHECKS",
+            columns=[
+                ("phone_number", "varchar(255)"),
+            ],
+            values=[
+                ('(112)460-2361x65894',),
+                ('807-662-2501x35953',),
+                ('(475)731-7388',),
+                ('001-274-976-7914',),
+                ('001-374-523-5313x4894',),
+                ('001-890-029-5781x0453',),
+                ('(104)288-2035',),
+                ('(607)302-7399',),
+                ('(888)525-5788',),
+                ('001-091-463-6827x23608',),
+                ('+1-719-180-2912',),
+                ('4031776749',),
+                ('(431)434-8587x558',),
+                ('(527)557-4929',),
+                ('632-820-4846',),
+                ('293.284.8346x9240',),
+                ('397-779-2835x593',),
+                ('8609814597',),
+                ('210-574-9910x971',),
+                ('4520621059',)
+            ],
+        )
+    )
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+              checks for {table_name}:
+                - invalid_count(phone_number) = 0:
+                    valid format: phone number
+            """
+    )
+    scan.execute(allow_warnings_only=True)
+
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
+
+
+def test_email_addresses(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="EMAIL_CHECKS",
+            columns=[
+                ("email_address", "varchar(255)"),
+            ],
+            values=[
+                ('michelle09@example.org',),
+                ('michael85@example.org',),
+                ('fordjohn@example.net',),
+                ('ehansen@example.com',),
+                ('brownmorgan@example.net',),
+                ('ymcdowell@example.net',),
+                ('pittmanbrian@example.com',),
+                ('ssnow@example.net',),
+                ('irollins@example.org',),
+                ('riggsjoseph@example.org',),
+                ('vincent49@example.net',),
+                ('meadowsrobert@example.net',),
+                ('timothy82@example.net',),
+                ('walkersusan@example.net',),
+                ('hammondlaura@example.org',),
+                ('ginajohnson@example.com',),
+                ('masonmelinda@example.net',),
+                ('wernerkelly@example.net',),
+                ('samuel13@example.net',),
+                ('moorekristin@example.com',)
+            ],
+        )
+    )
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+              checks for {table_name}:
+                - invalid_count(email_address) = 0:
+                    valid format: email
+            """
+    )
+    scan.execute()
+
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
+
+
+def test_duplicates(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="DUPLICATES",
+            columns=[
+                ("c01", "int"),
+            ],
+            values=[
+                (1,),
+                (2,),
+                (3,),
+                (3,),
+                (5,),
+                (6,),
+                (8,),
+                (8,),
+                (9,),
+                (10,),
+                (10,),
+                (12,),
+            ],
+        )
+    )
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+            - missing_count(c01) = 0
+            - duplicate_count(c01) = 3
+        """
+    )
+    scan.execute()
+
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
+
+
+def test_row_count(data_source_fixture: DataSourceFixture, tmp_path: Path):
+    import random
+
+    configured_row_count = random.randint(10, 100)
+
+    values = []
+
+    for i in range(configured_row_count):
+        values.append(tuple([5]))
+
+    table_name = data_source_fixture.ensure_test_table(
+        TestTable(
+            name="ROW_COUNT",
+            columns=[
+                ("c01", "int"),
+            ],
+            values=values,
+        )
+    )
+
+    scan = data_source_fixture.create_test_scan()
+    scan.add_sodacl_yaml_str(
+        f"""
+          checks for {table_name}:
+            - row_count = {configured_row_count}
+            - row_count between {configured_row_count - 10} and {configured_row_count + 10}
+        """
+    )
+    scan.execute()
+
+    scan.assert_all_checks_pass()
+    data_source_fixture._drop_test_table(table_name)
