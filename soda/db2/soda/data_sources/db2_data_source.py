f1fc936cb76ea2dc6ec433ed9006793dc8067496
4rahulae <167457911+4rahulae@users.noreply.github.com>
2024-04-23 15:30:21 -0400

    Update db2_data_source.py
    remove security from the 1st connection string

--- soda/db2/soda/data_sources/db2_data_source.py
+++ soda/db2/soda/data_sources/db2_data_source.py
@@ -69,7 +69,7 @@ class Db2DataSource(DataSource):
         self.update_schema(self.schema)
 
     def connect(self):
-        conn_str = f"DATABASE={self.database};HOSTNAME={self.host};PORT={self.port};UID={self.username};PWD={self.password};SECURITY={self.verify_ssl}"
+        conn_str = f"DATABASE={self.database};HOSTNAME={self.host};PORT={self.port};UID={self.username};PWD={self.password}"
         if self.security is not None:
             conn_str += f";SECURITY={self.security}"
         self.connection = ibm_db_dbi.connect(conn_str)
