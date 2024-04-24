c0db0d9592be1cd261d7d62f7bfa13849e84161b
Milan Lukac <milan@lukac.online>
2024-04-24 07:59:32 -0700

    Bump to 3.3.2

--- soda/athena/setup.py
+++ soda/athena/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-athena"
-package_version = "3.3.1"
+package_version = "3.3.2"
 description = "Soda Core Athena Package"
 
 requires = [
--- soda/contracts/setup.py
+++ soda/contracts/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-contracts"
-package_version = "3.3.1"
+package_version = "3.3.2"
 description = "Soda Core Contracts Package"
 
 requires = [f"soda-core=={package_version}", "jsonschema>=4.20.0"]
