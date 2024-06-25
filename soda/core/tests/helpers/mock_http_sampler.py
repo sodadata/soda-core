df40375e07e03bfa6983ae98cd964cd13a109a1e
Jarosław Załucki <jarek@Jarosaws-MacBook-Pro.local>
2024-06-25 23:34:11 +0200

    CLOUD-7845 Do not invoke failed rows API when no failed rows

---
+++ soda/core/tests/helpers/mock_http_sampler.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from typing import Tuple
+
+from soda.sampler.sample_context import SampleContext
+from soda.sampler.sample_ref import SampleRef
+from soda.sampler.http_sampler import HTTPSampler
+
+
+@dataclass
+class Sample:
+    rows: Tuple[Tuple]
+    sample_ref: SampleRef
+
+
+class MockHttpSampler(HTTPSampler):
+    def __init__(self):
+        self.samples = []
+        super().__init__("https://failed-rows.sampler.com")
+
+    def store_sample(self, sample_context: SampleContext) -> SampleRef:
+        sample_ref = super().store_sample(sample_context)
+        self.samples.append(Sample(rows=sample_context.sample.get_rows(), sample_ref=sample_ref))
+        return sample_ref
