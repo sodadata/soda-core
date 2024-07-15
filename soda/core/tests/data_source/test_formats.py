92e1098a87333f105a364b1dc4a1b670a216de73
Paul Holser <paul.holser@aclaimant.com>
2024-07-15 11:53:32 -0500

    ISO 8601 date should accept 24-hr times

--- soda/core/tests/data_source/test_formats.py
+++ soda/core/tests/data_source/test_formats.py
@@ -56,6 +56,8 @@ def test_formats(data_source_fixture: DataSourceFixture):
                 "07-04-1776",
                 "08-15-1620",
                 "09-20-1519",
+                "01-01-2100",
+                "01-01-1899",
             ],
             "failing_values": [
                 "",
@@ -121,6 +123,9 @@ def test_formats(data_source_fixture: DataSourceFixture):
                 "2020-02-08 09Z",
                 "2020-04-30",
                 "2020-04-30T00:00:00.000",
+                "2020-10-11 11:34",
+                "2100-01-01 14:36",
+                "1899-01-01 21:55",
             ],
             "failing_values": [
                 "",
@@ -129,6 +134,8 @@ def test_formats(data_source_fixture: DataSourceFixture):
                 "9999-01-01",
                 "2000-13-01",
                 "2000-01-32",
+                "2020-10-11 90:34",
+                "2100-01-01 14:67",
             ],
         },
     }
