a6f85fe04aa9563b9f233da01759785cc7ea5fd2
tombaeyens <mail@tombaeyens.be>
2024-09-11 09:22:49 +0200

    Bump to 3.3.21

--- soda/bigquery/setup.py
+++ soda/bigquery/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-bigquery"
-package_version = "3.3.20"
+package_version = "3.3.21"
 description = "Soda Core Bigquery Package"
 
 requires = [
