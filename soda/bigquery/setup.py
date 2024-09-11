2c8c5bdebfa6a4cd488a764a4fdd4b2590233464
tombaeyens <mail@tombaeyens.be>
2024-09-11 17:33:24 +0200

    Bump to 3.3.22

--- soda/bigquery/setup.py
+++ soda/bigquery/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-bigquery"
-package_version = "3.3.21"
+package_version = "3.3.22"
 description = "Soda Core Bigquery Package"
 
 requires = [
--- soda/duckdb/setup.py
+++ soda/duckdb/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-duckdb"
-package_version = "3.3.21"
+package_version = "3.3.22"
 description = "Soda Core Duckdb Package"
 
 requires = [f"soda-core=={package_version}", "duckdb==1.0.0"]
