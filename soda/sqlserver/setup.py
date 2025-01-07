6f169d4148e410d3a64e8dccc3ef79ecc22828e1
Jarosław Załucki <jarek@soda.io>
2025-01-07 12:31:06 +0100

    Bump to 3.4.4

--- soda/sqlserver/setup.py
+++ soda/sqlserver/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-sqlserver"
-package_version = "3.4.3"
+package_version = "3.4.4"
 description = "Soda Core SQL Server Package"
 
 requires = [f"soda-core=={package_version}", "pyodbc", "azure-identity~=1.17.1"]
