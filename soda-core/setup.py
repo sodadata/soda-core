865e4aeadc0550f11b928b54a3b0c18b847a59f1
Daniel Kühne <daniel.kuhne@soda.io>
2025-01-29 15:12:22 +0100

    add release metadata

--- soda-core/setup.py
+++ soda-core/setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
+import sys
+
 from setuptools import setup
+
+if sys.version_info < (3, 9):
+    print("Error: soda-core requires at least Python 3.9")
+    print("Error: Please upgrade your Python version to 3.9 or later")
+    sys.exit(1)
 
 package_name = "soda-core"
 package_version = "4.0.0b1"
@@ -15,7 +22,19 @@ setup(
     name=package_name,
     version=package_version,
     install_requires=requires,
+    author="Soda Data N.V.",
+    author_email="info@soda.io",
+    description="Soda core library & CLI",
     package_dir={"": "src"},
     package_data={"": ["**/*.json"]},
-    include_package_data=True
+    include_package_data=True,
+    python_requires=">=3.9",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.10",
+    ],
 )
