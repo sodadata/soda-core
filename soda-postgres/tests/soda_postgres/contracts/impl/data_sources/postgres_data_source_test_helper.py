b3514c171c6d24b0b2b556c6112d9bd0f2c701fd
Daniel Kühne <daniel.kuhne@soda.io>
2025-01-29 12:42:00 +0100

    make the PostgreSQL host configurable

--- soda-postgres/tests/soda_postgres/contracts/impl/data_sources/postgres_data_source_test_helper.py
+++ soda-postgres/tests/soda_postgres/contracts/impl/data_sources/postgres_data_source_test_helper.py
@@ -6,7 +6,6 @@ from soda_core.tests.helpers.data_source_test_helper import DataSourceTestHelper
 
 
 class PostgresDataSourceTestHelper(DataSourceTestHelper):
-
     def __init__(self):
         super().__init__()
 
@@ -18,13 +17,11 @@ class PostgresDataSourceTestHelper(DataSourceTestHelper):
             "type": "postgres",
             "name": "postgres_test_ds",
             "connection": {
-                "host": "localhost",
+                "host": os.getenv("POSTGRES_HOST", "localhost"),
                 "user": os.getenv("POSTGRES_USERNAME", "soda_test"),
                 "password": os.getenv("POSTGRES_PASSWORD"),
                 "port": int(os.getenv("POSTGRES_PORT", "5432")),
                 "database": self.database_name,
             },
-            "format_regexes": {
-                "single_digit_test_format": "^[0-9]$"
-            }
+            "format_regexes": {"single_digit_test_format": "^[0-9]$"},
         }
