b079e14710efccb1f55146a9b1b5994605fc4cc3
Milan Lukac <m1n0@users.noreply.github.com>
2025-02-20 01:23:36 -0800

    Update setup.py

--- soda/duckdb/setup.py
+++ soda/duckdb/setup.py
@@ -6,7 +6,7 @@ package_name = "soda-core-duckdb"
 package_version = "3.3.22"
 description = "Soda Core Duckdb Package"
 
-requires = [f"soda-core=={package_version}", "duckdb<1.1.0"]
+requires = [f"soda-core=={package_version}", "duckdb<1.3.0"]
 
 setup(
     name=package_name,
