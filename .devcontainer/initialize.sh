7f293de2af0321431cf2e74383e6cbe7e6d6a75d
Malo Jaffré <8469951+ghjklw@users.noreply.github.com>
2025-03-08 01:12:01 +0000

    Fix devcontainer initialize script

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -x
 
-# Install dependencies
-sudo apt-get update && sudo apt-get install libsasl2-dev
-
 # Create the virtual environment if it does not already exist
 if [ ! -d .venv ]; then
   scripts/recreate_venv.sh
