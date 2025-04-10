63d5e636ed7b757268c1c7dba949b59175a7cd46
Milan Lukac <milan@lukac.online>
2025-04-10 13:47:10 +0200

    Enforce strict model attributes

--- soda-core/src/soda_core/model/data_source/data_source_connection_properties.py
+++ soda-core/src/soda_core/model/data_source/data_source_connection_properties.py
@@ -3,9 +3,11 @@ from abc import ABC
 from pydantic import BaseModel
 
 
-class DataSourceConnectionProperties(BaseModel, ABC):
+class DataSourceConnectionProperties(
+    BaseModel,
+    ABC,
+    frozen=True,
+    extra="forbid",
+    validate_by_name=True,  # Allow to use both field names and aliases when populating from dict
+):
     pass
-
-    class Config:
-        # Allow to use both field names and aliases when populating from dict
-        validate_by_name = True
