4cfb95c1140cd4e9f3187c0f8d2da8c6d00c5a81
tombaeyens <mail@tombaeyens.be>
2025-04-30 10:40:57 +0200

    Added docs to TimeMachineDatetime explaining why it's there

--- soda-core/tests/helpers/a_time_machine.py
+++ soda-core/tests/helpers/a_time_machine.py
@@ -5,6 +5,15 @@ import pytest
 
 
 class TimeMachineDatetime(datetime.datetime):
+    """
+    Turns out that datetime.datetime.now cannot be mocked because it has native impl.
+    And it is possible to overwrite datetime.datetime with a class that inherits from datetime.datetime.
+    Especially the ordering of imports in the conftest.py is important. I renamed the file to a_time_machine.py so
+    that precommit would order the imports in the right order.
+    This solution requires that datetime.datetime is replaced with the class
+    TimeMachineDatetime(datetime.datetime) ** before ** any import datetime from datetime is perfomed by the
+    production code.
+    """
     NOW: Optional[datetime.datetime] = None
 
     @classmethod
