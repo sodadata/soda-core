8348f484cf55a64dbc5bac78fa248d9a270b1d4f
Niels Nuyttens <niels@soda.io>
2025-05-27 14:31:09 +0200

    Add the library version to file upload API calls. (#2288)
    * Add the library version to file upload API calls.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- soda-core/src/soda_core/common/soda_cloud.py
+++ soda-core/src/soda_core/common/soda_cloud.py
@@ -29,7 +29,7 @@ from soda_core.common.exceptions import (
 )
 from soda_core.common.logging_constants import Emoticons, ExtraKeys, soda_logger
 from soda_core.common.logs import Location, Logs
-from soda_core.common.version import SODA_CORE_VERSION
+from soda_core.common.version import SODA_CORE_VERSION, clean_soda_core_version
 from soda_core.common.yaml import SodaCloudYamlSource, YamlObject
 from soda_core.contracts.contract_publication import ContractPublicationResult
 from soda_core.contracts.contract_verification import (
@@ -277,6 +277,7 @@ class SodaCloud:
                     "Content-Type": "application/yaml",
                     "Is-V3": "true",
                     "File-Path": soda_cloud_file_path,
+                    "Soda-Library-Version": clean_soda_core_version(),
                 }
 
                 if file_size_in_bytes == 0:
--- soda-core/src/soda_core/common/version.py
+++ soda-core/src/soda_core/common/version.py
@@ -1,1 +1,15 @@
+import re
+
 SODA_CORE_VERSION = "4.0.0.b1"
+
+
+def clean_soda_core_version() -> str:
+    return clean_semver(SODA_CORE_VERSION)
+
+
+def clean_semver(version_str) -> str:
+    match = re.match(r"^(\d+\.\d+\.\d+)", version_str)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError(f"Invalid version format: {version_str}")
