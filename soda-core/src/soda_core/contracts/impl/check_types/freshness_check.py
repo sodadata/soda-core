2ced0a805e487e42b975bb5af1e422be4412819c
Milan Lukac <m1n0@users.noreply.github.com>
2025-06-10 16:15:57 +0200

    Fix freshness if no data, handle corner cases better (#2309)
    * Fix freshness if no data, hanlde corner cases better
    
    * no data->fail check
    
    * Fix test

--- soda-core/src/soda_core/contracts/impl/check_types/freshness_check.py
+++ soda-core/src/soda_core/contracts/impl/check_types/freshness_check.py
@@ -91,7 +91,13 @@ class FreshnessCheckImpl(CheckImpl):
         threshold_metric_name: str = f"freshness_in_{self.unit}s"
 
         threshold_value: Optional[float] = None
-        if data_timestamp_utc and max_timestamp_utc:
+        if max_timestamp_utc is None:
+            outcome = CheckOutcome.FAILED
+
+        elif data_timestamp_utc is not None:
+            logger.debug(
+                f"Calculating freshness using '{max_timestamp}' as 'max' and '{data_timestamp}' as 'now' values"
+            )
             freshness = data_timestamp_utc - max_timestamp_utc
             freshness_in_seconds = freshness.total_seconds()
 
@@ -111,6 +117,8 @@ class FreshnessCheckImpl(CheckImpl):
                 else:
                     outcome = CheckOutcome.FAILED
 
+        freshness_str: Optional[str] = str(freshness) if freshness is not None else None
+
         return FreshnessCheckResult(
             contract=contract,
             check=self._build_check_info(),
@@ -121,27 +129,32 @@ class FreshnessCheckImpl(CheckImpl):
             max_timestamp_utc=max_timestamp_utc,
             data_timestamp=data_timestamp,
             data_timestamp_utc=data_timestamp_utc,
-            freshness=str(freshness),
+            freshness=freshness_str,
             freshness_in_seconds=freshness_in_seconds,
             unit=self.unit,
         )
 
     def _get_max_timestamp(self, measurement_values: MeasurementValues) -> Optional[datetime]:
         max_timestamp: Optional[datetime] = measurement_values.get_value(self.max_timestamp_metric)
-        if not isinstance(max_timestamp, datetime):
+        if max_timestamp is None:
+            logger.warning(
+                f"Freshness metric '{self.max_timestamp_metric.type}' for column '{self.column}' "
+                f"returned no value. Does the table or partition have rows?"
+            )
+            return None
+        elif not isinstance(max_timestamp, datetime):
+            logger.debug(
+                f"Attempting to convert freshness value '{max_timestamp}' of data type '{type(max_timestamp).__name__}' to datetime"
+            )
             if isinstance(max_timestamp, date):
                 max_timestamp = datetime.combine(max_timestamp, datetime.min.time())
             elif isinstance(max_timestamp, str):
                 max_timestamp = convert_str_to_datetime(max_timestamp)
-            else:
-                logger.error(
-                    f"Freshness metric '{self.max_timestamp_metric.type}' for column '{self.column}' "
-                    f"has an invalid data type '({type(max_timestamp).__name__})'. "
-                    f"Is the column a timestamp or a timestamp-compatible type?"
-                )
 
         if not isinstance(max_timestamp, datetime):
-            logger.error(f"Freshness column '{self.column}' does not have timestamp values: '{max_timestamp}'")
+            logger.error(
+                f"Freshness column '{self.column}' returned value '{max_timestamp}' of data type '{type(max_timestamp).__name__}' which is not a datetime or datetime-compatible type."
+            )
             max_timestamp = None
 
         return max_timestamp
