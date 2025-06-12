0ad454c7507a74e2430cd67b695113e936495c99
Milan Lukac <milan@lukac.online>
2025-06-12 14:00:00 +0200

    Bump to 3.5.5

--- soda/postgres/setup.py
+++ soda/postgres/setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_namespace_packages, setup
 
 package_name = "soda-core-postgres"
-package_version = "3.5.4"
+package_version = "3.5.5"
 # TODO Add proper description
 description = "Soda Core Postgres Package"
 
