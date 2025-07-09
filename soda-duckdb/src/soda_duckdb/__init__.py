0a7f96679f69233348426d09a6e68d66835fdacd
Milan Lukac <milan@lukac.online>
2025-07-09 10:20:14 +0200

    Nicer duckdb imports

--- soda-duckdb/src/soda_duckdb/__init__.py
+++ soda-duckdb/src/soda_duckdb/__init__.py
@@ -1,5 +1,6 @@
-from soda_duckdb.common.data_sources.duckdb_data_source import DuckDBDataSourceImpl as DuckDBDataSource
-
+from soda_duckdb.common.data_sources.duckdb_data_source import (
+    DuckDBDataSourceImpl as DuckDBDataSource,
+)
 
 __all__ = [
     "DuckDBDataSource",
