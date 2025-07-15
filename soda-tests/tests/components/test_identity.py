b5371846c01901db7072e60aaa46e608300d464e
Milan Lukac <milan@lukac.online>
2025-07-15 11:49:37 +0200

    Add test for identity

---
+++ soda-tests/tests/components/test_identity.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timezone
+
+from freezegun import freeze_time
+from helpers.data_source_test_helper import DataSourceTestHelper
+from helpers.test_table import TestTableSpecification
+from soda_core.contracts.contract_verification import (
+    CheckResult,
+    ContractVerificationResult,
+)
+
+test_table_specification = (
+    TestTableSpecification.builder()
+    .table_purpose("identity")
+    .column_integer("id")
+    .column_timestamp_tz("created_at")
+    .rows(
+        rows=[
+            (1, datetime(year=2025, month=1, day=1, hour=0, minute=0, second=0, tzinfo=timezone.utc)),
+            (2, datetime(year=2025, month=1, day=2, hour=2, minute=0, second=0, tzinfo=timezone.utc)),
+            (3, datetime(year=2025, month=1, day=3, hour=4, minute=0, second=0, tzinfo=timezone.utc)),
+        ]
+    )
+    .build()
+)
+
+
+def test_identity_stable(data_source_test_helper: DataSourceTestHelper):
+    """
+    This test verifies that the identity mechanism does not change over time.
+
+    This is achieved by hardcoding the identity in the test.
+    """
+    test_table = data_source_test_helper.ensure_test_table(test_table_specification)
+
+    with freeze_time(datetime(year=2025, month=1, day=3, hour=10, minute=0, second=0, tzinfo=timezone.utc)):
+        contract_verification_result: ContractVerificationResult = data_source_test_helper.assert_contract_pass(
+            test_table=test_table,
+            contract_yaml_str=f"""
+                check_attributes:
+                    description: "Test description"
+                columns:
+                    - name: id
+                      checks:
+                        - aggregate:
+                            function: avg
+                            threshold:
+                                must_be: 2
+                        - invalid:
+                            valid_values: ['1', '2', '3']
+                        - missing:
+                        - duplicate:
+                    - name: created_at
+                checks:
+                    - schema:
+                    - freshness:
+                        column: created_at
+                        threshold:
+                            must_be_less_than: 12
+            """,
+        )
+        check_results: list[CheckResult] = contract_verification_result.check_results
+
+        assert check_results[0].check.identity == "40add3fc"
+        assert check_results[1].check.identity == "374236b5"
+        assert check_results[2].check.identity == "0032a191"
+        assert check_results[3].check.identity == "cdea1973"
+        assert check_results[4].check.identity == "88220ce2"
+        assert check_results[5].check.identity == "87ae1449"
