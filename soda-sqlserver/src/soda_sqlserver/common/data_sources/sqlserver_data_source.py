84006b02ccf3989a9174f7c19c376d46ac45d7d6
Paul Teehan <paul.teehan+1@gmail.com>
2025-08-04 01:41:09 +0200

    Fix date formatting in sqlserver

--- soda-sqlserver/src/soda_sqlserver/common/data_sources/sqlserver_data_source.py
+++ soda-sqlserver/src/soda_sqlserver/common/data_sources/sqlserver_data_source.py
@@ -110,3 +110,8 @@ class SQLServerSqlDialect(SqlDialect):
             DBDataType.TIMESTAMP_TZ: "datetimeoffset",
             DBDataType.BOOLEAN: "bit",
         }
+
+    def literal_date(self, date: date):
+        """Technically dates can be passed directly as strings, but this is more explicit."""
+        date_string = date.strftime("%Y-%m-%d")
+        return f"CAST('{date_string}' AS DATE)"
