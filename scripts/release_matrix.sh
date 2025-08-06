5a5cc94f24bc90d44194eb499a2b0c4d4ddda6df
Niels Nuyttens <niels@soda.io>
2025-08-06 13:08:27 +0200

    Re-enable release of `soda-databricks` (my bad)

--- scripts/release_matrix.sh
+++ scripts/release_matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "["; find . -maxdepth 1 -mindepth 1 -type d -name "soda*" ! -name "soda-databricks" -printf '"%f",' | sed 's/,$//'; printf "]"
+printf "["; find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -printf '"%f",' | sed 's/,$//'; printf "]"
