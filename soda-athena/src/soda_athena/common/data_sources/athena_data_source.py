8d81d522c9165d5a41d4a9dc353566fdf72e2552
Niels Bylois <niels.bylois@soda.io>
2025-08-14 14:41:06 +0200

    Monkeypatch the freezegun for AWS connections

--- soda-athena/src/soda_athena/common/data_sources/athena_data_source.py
+++ soda-athena/src/soda_athena/common/data_sources/athena_data_source.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 from typing import Optional
 
 from soda_athena.common.data_sources.athena_data_source_connection import (
@@ -28,6 +29,20 @@ class AthenaDataSourceImpl(DataSourceImpl, model_class=AthenaDataSourceModel):
             name=self.data_source_model.name, connection_properties=self.data_source_model.connection_properties
         )
 
+    # def execute_query(self, sql: str) -> QueryResult:
+    #     # Athena does not play well with freezegun.
+    #     # The datasources requires a timestamp in the query (done automatically), this timestamp must be in sync with AWS servers
+    #     # This is not the case when using freezegun.
+    #     # We need to disable freezegun for this method.
+
+    #     # TODO: Refactor this so it's in a proper place. It shouldn't live here.
+    #     # now = datetime.now()
+    #     # if now < datetime(year=2025, month=8, day=1, hour=0, minute=0, second=0):
+    #     #     with freezegun.freeze_time(freezegun.api.real_datetime.now(tz=timezone.utc)):
+    #     #         result = self.connection.execute_query(sql=sql)
+    #     #     return result
+    #     return self.connection.execute_query(sql=sql)
+
 
 class AthenaSqlDialect(SqlDialect):
     def get_contract_type_dict(self) -> dict[str, str]:
@@ -44,9 +59,18 @@ class AthenaSqlDialect(SqlDialect):
 
     def get_sql_type_dict(self) -> dict[str, str]:
         base_sql_type_dict = super().get_sql_type_dict()
-        base_sql_type_dict[DBDataType.TIMESTAMP] = "timestamp(3)"
-        base_sql_type_dict[DBDataType.TIMESTAMP_TZ] = "timestamp(3)"
+        # base_sql_type_dict[DBDataType.TIMESTAMP] = "timestamp(3)"
+        # base_sql_type_dict[DBDataType.TIMESTAMP_TZ] = "timestamp(3)"
         return base_sql_type_dict
 
     def quote_default(self, identifier: Optional[str]) -> Optional[str]:
         return identifier
+
+    def literal_datetime(self, datetime: datetime):
+        return f"From_iso8601_timestamp('{datetime.isoformat()}')"
+
+    def literal_datetime_with_tz(self, datetime: datetime):
+        # Can be overloaded if the subclass does not support timezones (may have to do conversion yourself)
+        # We assume that all timestamps are stored in UTC.
+        # See Fabric for an example
+        return self.literal_datetime(datetime)
--- soda-tests/tests/conftest.py
+++ soda-tests/tests/conftest.py
@@ -1,6 +1,43 @@
+import freezegun
 from helpers.test_fixtures import *  # noqa: F401
 from soda_core.common.logging_configuration import configure_logging
 
 
 def pytest_sessionstart(session) -> None:
     configure_logging(verbose=True)
+
+
+def monkey_patch_freezegun_for_google_api():
+    def utcnow():
+        if freezegun.api._should_use_real_time():
+            result = freezegun.api.real_datetime.utcnow()
+        else:
+            result = freezegun.api.FakeDatetime._time_to_freeze()
+        return freezegun.api.datetime_to_fakedatetime(result)
+
+    def now(tz=None):
+        if freezegun.api._should_use_real_time():
+            result = freezegun.api.real_datetime.now(tz=tz)
+        else:
+            result = freezegun.api.FakeDatetime._time_to_freeze()
+        return freezegun.api.datetime_to_fakedatetime(result)
+
+    setattr(freezegun.api.FakeDatetime, "utcnow", utcnow)
+    setattr(freezegun.api.FakeDatetime, "now", now)
+
+
+monkey_patch_freezegun_for_google_api()
+
+
+my_ignore_list = [
+    "pyathena",
+    "boto3",
+    "botocore",
+    "boto3",
+    "botocore",
+    "botocore.auth",
+    "botocore.credentials",
+    "botocore.awsrequest",
+]
+freezegun.configure(extend_ignore_list=my_ignore_list)
+freeze_time = freezegun.freeze_time
