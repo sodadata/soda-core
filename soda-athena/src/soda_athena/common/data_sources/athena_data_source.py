7c0946b39760cf2fd6a6f4681a0bc11df978c098
Niels Bylois <niels.bylois@soda.io>
2025-08-18 18:05:32 +0200

    Updated typings for Athena

--- soda-athena/src/soda_athena/common/data_sources/athena_data_source.py
+++ soda-athena/src/soda_athena/common/data_sources/athena_data_source.py
@@ -155,17 +155,20 @@ class AthenaSqlDialect(SqlDialect):
         base_contract_type_dict = super().get_contract_type_dict()
         base_contract_type_dict[DBDataType.TEXT] = "varchar"
         base_contract_type_dict[DBDataType.BOOLEAN] = "boolean"
-        base_contract_type_dict[DBDataType.INTEGER] = "int"
+        base_contract_type_dict[DBDataType.INTEGER] = "integer"
         base_contract_type_dict[DBDataType.DECIMAL] = "decimal"
         base_contract_type_dict[DBDataType.DATE] = "date"
         base_contract_type_dict[DBDataType.TIME] = "date"
-        base_contract_type_dict[DBDataType.TIMESTAMP] = "timestamp"
-        base_contract_type_dict[DBDataType.TIMESTAMP_TZ] = "timestamp"
+        base_contract_type_dict[DBDataType.TIMESTAMP] = "timestamp(3)"
+        base_contract_type_dict[DBDataType.TIMESTAMP_TZ] = "timestamp(3)"
         return base_contract_type_dict
 
     def get_sql_type_dict(self) -> dict[str, str]:
         base_sql_type_dict = super().get_sql_type_dict()
         base_sql_type_dict[DBDataType.TEXT] = "string"
+        base_sql_type_dict[DBDataType.INTEGER] = "int"
+        base_sql_type_dict[DBDataType.TIMESTAMP] = "timestamp"
+        base_sql_type_dict[DBDataType.TIMESTAMP_TZ] = "timestamp"
         return base_sql_type_dict
 
     def quote_default(self, identifier: Optional[str]) -> Optional[str]:
