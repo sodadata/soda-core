00651063698c646d0d06ae283c0b0bd336ed1ca7
tombaeyens <mail@tombaeyens.be>
2025-08-26 17:31:23 +0200

    Improved test table metadata

--- soda-tests/tests/features/test_table_metadata.py
+++ soda-tests/tests/features/test_table_metadata.py
@@ -1,7 +1,11 @@
 from helpers.data_source_test_helper import DataSourceTestHelper
 from helpers.test_table import TestTableSpecification
 from soda_core.common.data_source_results import QueryResult
-from soda_core.common.metadata_types import ColumnMetadata, SodaDataTypeName
+from soda_core.common.metadata_types import (
+    ColumnMetadata,
+    SodaDataTypeName,
+    SqlDataType,
+)
 from soda_core.common.sql_dialect import SqlDialect
 from soda_core.common.statements.metadata_columns_query import MetadataColumnsQuery
 
@@ -39,83 +43,76 @@ def test_table_metadata(data_source_test_helper: DataSourceTestHelper):
 
     actual_txt_default: ColumnMetadata = actual_columns[0]
     assert actual_txt_default.column_name == "varchar_default"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_txt_default.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.VARCHAR),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.VARCHAR)),
+        actual=actual_txt_default.sql_data_type,
     )
-    assert actual_txt_default.sql_data_type.character_maximum_length is None
-    assert actual_txt_default.sql_data_type.numeric_precision is None
-    assert actual_txt_default.sql_data_type.numeric_scale is None
-    assert actual_txt_default.sql_data_type.datetime_precision is None
 
     actual_txt_w_length: ColumnMetadata = actual_columns[1]
     assert actual_txt_w_length.column_name == "varchar_w_length"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_txt_w_length.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.VARCHAR),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.VARCHAR),
+            character_maximum_length=255,
+        ),
+        actual=actual_txt_w_length.sql_data_type,
     )
-    assert actual_txt_w_length.sql_data_type.character_maximum_length == 255
-    assert actual_txt_w_length.sql_data_type.numeric_precision is None
-    assert actual_txt_w_length.sql_data_type.numeric_scale is None
-    assert actual_txt_w_length.sql_data_type.datetime_precision is None
 
     actual_integer_default: ColumnMetadata = actual_columns[2]
     assert actual_integer_default.column_name == "integer_default"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_integer_default.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.INTEGER),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.INTEGER),
+        ),
+        actual=actual_integer_default.sql_data_type,
     )
-    assert actual_integer_default.sql_data_type.character_maximum_length is None
-    assert actual_integer_default.sql_data_type.numeric_precision is None
-    assert actual_integer_default.sql_data_type.numeric_scale is None
-    assert actual_integer_default.sql_data_type.datetime_precision is None
 
     actual_numeric_default: ColumnMetadata = actual_columns[3]
     assert actual_numeric_default.column_name == "numeric_default"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_numeric_default.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+        ),
+        actual=actual_numeric_default.sql_data_type,
     )
-    assert actual_numeric_default.sql_data_type.character_maximum_length is None
-    assert actual_numeric_default.sql_data_type.numeric_precision is None
-    assert actual_numeric_default.sql_data_type.numeric_scale is None
-    assert actual_numeric_default.sql_data_type.datetime_precision is None
 
     actual_numeric_w_precision: ColumnMetadata = actual_columns[4]
     assert actual_numeric_w_precision.column_name == "numeric_w_precision"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_numeric_w_precision.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+            numeric_precision=10,
+            numeric_scale=0,
+        ),
+        actual=actual_numeric_w_precision.sql_data_type,
     )
-    assert actual_numeric_w_precision.sql_data_type.character_maximum_length is None
-    assert actual_numeric_w_precision.sql_data_type.numeric_precision == 10
-    assert actual_numeric_w_precision.sql_data_type.numeric_scale == 0
-    assert actual_numeric_w_precision.sql_data_type.datetime_precision is None
 
     actual_numeric_w_precision_and_scale: ColumnMetadata = actual_columns[5]
     assert actual_numeric_w_precision_and_scale.column_name == "numeric_w_precision_and_scale"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_numeric_w_precision_and_scale.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+            numeric_precision=10,
+            numeric_scale=2,
+        ),
+        actual=actual_numeric_w_precision_and_scale.sql_data_type,
     )
-    assert actual_numeric_w_precision_and_scale.sql_data_type.character_maximum_length is None
-    assert actual_numeric_w_precision_and_scale.sql_data_type.numeric_precision == 10
-    assert actual_numeric_w_precision_and_scale.sql_data_type.numeric_scale == 2
-    assert actual_numeric_w_precision_and_scale.sql_data_type.datetime_precision is None
 
     actual_ts_default: ColumnMetadata = actual_columns[6]
     assert actual_ts_default.column_name == "ts_default"
-    assert sql_dialect.data_type_names_are_same_or_synonym(
-        actual_ts_default.sql_data_type.name,
-        sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.NUMERIC),
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.TIMESTAMP),
+        ),
+        actual=actual_ts_default.sql_data_type,
     )
-    assert actual_ts_default.sql_data_type.character_maximum_length is None
-    assert actual_ts_default.sql_data_type.numeric_precision is None
-    assert actual_ts_default.sql_data_type.numeric_scale is None
-    # TODO fix this
-    # assert actual_ts_default.sql_data_type.datetime_precision is None
 
-    # TODO
-    # .column_timestamp(name="ts_w_precision", datetime_precision=2)
-    # .column_timestamp_tz(name="ts_tz_default")
-    # .column_timestamp_tz(name="ts_tz_w_precision", datetime_precision=4)
+    actual_ts_w_precision: ColumnMetadata = actual_columns[7]
+    assert actual_ts_w_precision.column_name == "ts_w_precision"
+    assert sql_dialect.is_same_data_type_for_schema_check(
+        expected=SqlDataType(
+            name=sql_dialect.get_sql_data_type_name_for_soda_data_type_name(SodaDataTypeName.TIMESTAMP),
+            datetime_precision=2,
+        ),
+        actual=actual_ts_w_precision.sql_data_type,
+    )
