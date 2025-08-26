8624b3fc23e859f296ff719db5dec0ca64634363
Daniel Kühne <daniel.kuhne@soda.io>
2025-08-26 12:40:21 +0200

    PLATL-128 - Refactor soda-core v4 CI to use AWS Secretsmanager for secrets

--- scripts/test_matrix.sh
+++ scripts/test_matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -not -name "soda-databricks" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
+printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
