704eaab2dfba2539b85d427edc63ef019e43a7e0
Paul Teehan <paul.teehan+1@gmail.com>
2025-08-27 13:49:16 +0200

    Restore MS dbs to CI

--- scripts/test_matrix.sh
+++ scripts/test_matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -not -name "soda-databricks" -not -name "soda-fabric" -not -name "soda-synapse" -not -name "soda-sqlserver" -not -name "soda-duckdb" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
+printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -not -name "soda-databricks" -not -name "soda-duckdb" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
