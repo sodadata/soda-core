ca10c994ed5b1f84d523e5af1228d678213cc5ff
Paul Teehan <paul.teehan+1@gmail.com>
2025-09-22 15:25:11 +0200

    Add oracle to CI

--- scripts/test_matrix.sh
+++ scripts/test_matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
+EXT_SOURCES="oracle"  # data sources implemented in soda-extensions
+printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf ",\"$EXT_SOURCES\"]"
