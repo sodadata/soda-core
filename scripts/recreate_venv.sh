224b9756463112337647b26e6f48b0a11375b4f1
Paul Teehan <paul.teehan+1@gmail.com>
2025-09-29 00:12:23 +0200

    Handle dependencies correctly

--- scripts/recreate_venv.sh
+++ scripts/recreate_venv.sh
@@ -9,7 +9,6 @@ pip install "$(grep pip-tools < dev-requirements.in )"
 pip-compile dev-requirements.in
 pip install -r dev-requirements.txt
 
-cat requirements.txt | while read requirement || [[ -n $requirement ]];
-do
-   pip install -e $requirement
-done
+# Install all requirements with -e flag
+# Pass them all to pip at once so it can resolve dependencies and install in the correct order
+pip install $(sed 's/^/-e /' requirements.txt | tr '\n' ' ')
