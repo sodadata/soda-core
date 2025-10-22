a5ca477733745ea0b1d38a46e650d741427663f1
Milan Lukac <m1n0@users.noreply.github.com>
2025-10-22 21:29:05 +0200

    Migration: bugfixes (#2455)

--- soda-core/src/soda_core/common/utils.py
+++ soda-core/src/soda_core/common/utils.py
@@ -28,3 +28,20 @@ def format_items(items: Union[List[str], Dict[str, str]], verbose: bool = False,
 def to_camel_case(snake_str: str) -> str:
     components = snake_str.split("_")
     return components[0] + "".join(x.title() for x in components[1:])
+
+
+# Taken from distutils.util for compatibility reasons. Removed in Python 3.12
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return 1
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
