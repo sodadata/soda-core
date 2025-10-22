449e29e26e8c43212f850c2341efa69ac57b2b15
Niels Bylois <niels.bylois@soda.io>
2025-10-22 14:57:44 +0200

    Fixing import

--- soda-sparkdf/src/soda_sparkdf/__init__.py
+++ soda-sparkdf/src/soda_sparkdf/__init__.py
@@ -1,7 +1,7 @@
-from soda_duckdb.common.data_sources.duckdb_data_source import (
-    DuckDBDataSourceImpl as DuckDBDataSource,
+from soda_sparkdf.common.data_sources.sparkdf_data_source import (
+    SparkDataFrameDataSourceImpl as SparkDataFrameDataSource,
 )
 
 __all__ = [
-    "DuckDBDataSource",
+    "SparkDataFrameDataSource",
 ]
