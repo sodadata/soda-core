a515f873b049309095d7c1aa9f40754e7e8fdad0
paulteehan <45394252+paulteehan@users.noreply.github.com>
2025-11-20 23:07:30 +0100

    Dremio local development infra (#2481)
    * exclude local dremio data
    
    * local dremio instance
    
    * Update config script and instructions
    
    * add scripts
    
    * add utf params
    
    * remove unfinished
    
    * remove unneeded
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Exclude dremio from CI
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- scripts/test_matrix.sh
+++ scripts/test_matrix.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
+# temporarily exclude soda-dremio from the matrix - tests not passing yet 
+printf "[";find . -maxdepth 1 -mindepth 1 -type d -name "soda*" -not -name "soda-core" -not -name "soda-tests" -not -name "soda-dremio" -printf '"%f",' | sed 's/,$//;s/soda-//g';printf "]"
