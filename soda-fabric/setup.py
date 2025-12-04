074ad0497cfd8f78ae27eb1925ebe9b7efc330d0
Niels Nuyttens <niels@soda.io>
2025-12-04 20:18:33 +0100

    Bump to 4.0.4b19

--- soda-fabric/setup.py
+++ soda-fabric/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 package_name = "soda-fabric"
-package_version = "4.0.4b18"
+package_version = "4.0.4b19"
 description = "Soda Fabric V4"
 
 requires = [
--- soda-sqlserver/setup.py
+++ soda-sqlserver/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 package_name = "soda-sqlserver"
-package_version = "4.0.4b18"
+package_version = "4.0.4b19"
 description = "Soda SQL Server V4"
 
 requires = [
