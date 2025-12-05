88abeeaf475191a6d391c81e4dd628be17fd69da
Milan Lukac <m1n0@users.noreply.github.com>
2025-12-05 15:05:37 +0100

    fix: Sampling prevent double sampling subqueries (#2508)
    * fix: Sampling prevent fouble sampling subqueries
    
    * fix tests
    
    * fix tests

--- soda-core/src/soda_core/common/sql_utils.py
+++ soda-core/src/soda_core/common/sql_utils.py
@@ -54,9 +54,12 @@ def apply_sampling_to_sql(
 
     # FROM sources (top-level, CTE bodies, nested subqueries)
     # Keep track of CTEs and skip them as they are already sampled at their definition
+    # Skip subqueries as they are sampled at their definition. We skip the FROM SUBQUERY part, the FROM within the subquery is handled when we process that subquery separately.
     ctes = {cte.alias_or_name for cte in tree.find_all(exp.CTE)}
     for from_ in tree.find_all(exp.From):
         if isinstance(from_.this, exp.Table) and from_.this.alias_or_name in ctes:
+            continue
+        if isinstance(from_.this, exp.Subquery):
             continue
 
         attach_sample_to_relation(from_.this, sampler_limit, sampler_type)
@@ -65,4 +68,4 @@ def apply_sampling_to_sql(
     for join in tree.find_all(exp.Join):
         attach_sample_to_relation(join.this, sampler_limit, sampler_type)
 
-    return tree.sql(dialect=write_dialect) if write_dialect else tree.sql()
+    return tree.sql(dialect=write_dialect, pretty=True) if write_dialect else tree.sql(pretty=True)
