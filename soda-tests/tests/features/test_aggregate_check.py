f59fdeb072a40cd238516c82cd54b65be99bd29e
Paul Teehan <paul.teehan@soda.io>
2025-12-12 12:47:43 +0100

    formatting

--- soda-tests/tests/features/test_aggregate_check.py
+++ soda-tests/tests/features/test_aggregate_check.py
@@ -1,4 +1,5 @@
 import math
+
 from helpers.data_source_test_helper import DataSourceTestHelper
 from helpers.mock_soda_cloud import MockResponse
 from helpers.test_functions import get_diagnostic_value
@@ -157,7 +158,7 @@ def test_aggregate_function_avg_length_alt(data_source_test_helper: DataSourceTe
     # The platform will generate 'avglength' instead of 'avg_length', check to make sure this is supported
     check_result: CheckResult = contract_verification_result.check_results[0]
     result_value = get_diagnostic_value(check_result, "avglength")  # nb avgLength becomes avglength in results
-    assert (math.isclose(result_value, 2.2)) or (result_value == 2)  # some DBs will return ints    
+    assert (math.isclose(result_value, 2.2)) or (result_value == 2)  # some DBs will return ints
 
 
 def test_aggregate_function_avg_with_missing(data_source_test_helper: DataSourceTestHelper):
