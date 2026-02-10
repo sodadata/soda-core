1cea2573b8d0c51fdcd6f69f3403743626b88845
Paul Teehan <paul.teehan@gmail.com>
2026-02-10 18:36:42 +0100

    Remove unnecessary file

--- soda-trino/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-from helpers.test_fixtures import *  # noqa: F401
-from soda_core.common.logging_configuration import configure_logging
-
-
-def pytest_sessionstart(session) -> None:
-    configure_logging(verbose=True)
