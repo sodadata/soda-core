b853c301f1224c1542bc478f48fb6340de88efd6
Milan Lukac <milan@lukac.online>
2026-02-16 15:41:07 +0100

    test check overrides as well

--- soda-tests/tests/feature/test_column_expression.py
+++ soda-tests/tests/feature/test_column_expression.py
@@ -76,3 +76,61 @@ def test_column_level_column_expression_metric_checks_fail(data_source_test_help
         "datasetRowsTested": 5,
         "checkRowsTested": 5,
     }
+
+
+def test_check_level_column_expression_metric_checks_fail(data_source_test_helper: DataSourceTestHelper):
+    test_table = data_source_test_helper.ensure_test_table(test_table_specification)
+
+    data_source_test_helper.enable_soda_cloud_mock(
+        [
+            MockResponse(status_code=200, json_object={"fileId": "a81bc81b-dead-4e5d-abff-90865d1e13b1"}),
+        ]
+    )
+
+    result = data_source_test_helper.assert_contract_fail(
+        test_table=test_table,
+        contract_yaml_str=f"""
+            columns:
+              - name: id
+                column_expression: "CRASH_IF_USED"
+                checks:
+                  - missing:
+                        column_expression: '"id"::varchar'
+                        missing_format:
+                            regex: '^-\d+$'
+                            name: positive integers only
+              - name: age_str
+                column_expression: "CRASH_IF_USED"
+                checks:
+                  - aggregate:
+                        column_expression: '"age_str"::integer'
+                        function: min
+                        threshold:
+                            must_be: 20
+
+
+        """,
+    )
+
+    for check in result.check_results:
+        assert check.outcome == CheckOutcome.FAILED
+
+    soda_core_insert_scan_results_command = data_source_test_helper.soda_cloud.requests[1].json
+
+    # Missing check
+    check_json: dict = soda_core_insert_scan_results_command["checks"][0]
+    assert check_json["diagnostics"]["v4"] == {
+        "type": "missing",
+        "failedRowsCount": 2,
+        "failedRowsPercent": 40.0,
+        "datasetRowsTested": 5,
+        "checkRowsTested": 5,
+    }
+
+    # Aggregate check
+    check_json: dict = soda_core_insert_scan_results_command["checks"][1]
+    assert check_json["diagnostics"]["v4"] == {
+        "type": "aggregate",
+        "datasetRowsTested": 5,
+        "checkRowsTested": 5,
+    }
