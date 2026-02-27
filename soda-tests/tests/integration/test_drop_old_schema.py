51cdb0076b682388d4b07d39520de7a27a665bf3
Niels Bylois <niels.bylois@soda.io>
2026-02-27 23:13:08 +0100

    Fix: Update schema cleanup script (part 1)

--- soda-tests/tests/integration/test_drop_old_schema.py
+++ soda-tests/tests/integration/test_drop_old_schema.py
@@ -16,8 +16,10 @@ logger: logging.Logger = soda_logger
 DATASOURCES_TO_RUN = [
     "postgres",  # Postgres is not required, but just to verify that the test works (also for local clean up)
     "snowflake",
-    # "athena",
+    "athena",
     "bigquery",
+    "redshift",
+    "synapse",
 ]
 # Only drop schemma's starting with these prefixes.
 LIST_OF_PREFIXES_TO_DROP = ["soda_diagnostics_", "ALTERNATE_DWH_", "ci_", "my_dwh_"]
@@ -29,13 +31,16 @@ def determine_if_schema_needs_to_be_dropped(schema_name: str) -> bool:
     # We determine if the schema needs to be dropped by checking if the schema name contains a date that we can parse. If it's older than 2 days, we drop it.
     try:
         schema_name = schema_name.lower()
-        if "soda_diagnostics" in schema_name:
+        must_have_date: bool = False
+        if schema_name.lower().startswith("soda_diagnostics_"):
             potential_date_string: str = schema_name[
                 len("soda_diagnostics_") + 9 : -7
             ]  # soda_diagnostics_0db10c31_20251119_093446
-        elif "ALTERNATE_DWH" in schema_name:
+            # Only drop the schema if it has a date
+            must_have_date = True
+        elif schema_name.upper().startswith("ALTERNATE_DWH_"):
             potential_date_string: str = schema_name[len("ALTERNATE_DWH_") + 9 :]  # ALTERNATE_DWH_0db10c31_20251119
-        elif "ci_" in schema_name:
+        elif schema_name.lower().startswith("ci_"):
             # There are too many structures for this to be done in a simple way, so we have to try all the possibilities for dates.
             # Start from the beginning and try every substring of 8 characters (after removing underscore)
             no_underscore_schema_name: str = schema_name.replace("_", "")
@@ -53,7 +58,7 @@ def determine_if_schema_needs_to_be_dropped(schema_name: str) -> bool:
                     continue
             if not found_date:
                 return False  # Do not drop the schema if we cannot find a date in the schema name. (safeguard, we can manually remove these schemas if needed)
-        elif "my_dwh_" in schema_name:
+        elif schema_name.lower().startswith("my_dwh_"):
             first_part_index = len("my_dwh_") + 6
             second_part_index = first_part_index + 8
             potential_date_string: str = schema_name[
@@ -64,7 +69,9 @@ def determine_if_schema_needs_to_be_dropped(schema_name: str) -> bool:
         date_obj: datetime.datetime = parse(potential_date_string, fuzzy=False)
         return date_obj < datetime.datetime.now() - datetime.timedelta(days=2)  # Remove everything older than 2 days.
     except ValueError:  # Cannot parse the date, so there is none -> drop it.
-        return True
+        return (
+            not must_have_date
+        )  # Only drop the schema if it should not have a date. E.g. we don't want to drop the schema if it's a soda_diagnostics_my_dwh_test schema.
 
 
 def test_drop_old_schemas(data_source_test_helper: DataSourceTestHelper):
@@ -80,20 +87,24 @@ def test_drop_old_schemas(data_source_test_helper: DataSourceTestHelper):
     schemas_query_sql = dialect.build_schemas_metadata_query_str(table_namespace=table_namespace)
     query_result: QueryResult = data_source_test_helper.data_source_impl.execute_query(schemas_query_sql)
     schema_names: list[str] = [row[0] for row in query_result.rows]
+    num_deleted_schemas: int = 0
     for schema_name in schema_names:
-        if any(schema_name.lower().startswith(prefix) for prefix in LIST_OF_PREFIXES_TO_DROP):
-            if not any(schema_name.lower().startswith(prefix) for prefix in LIST_OF_EXEMPTIONS):
+        if any(schema_name.lower().startswith(prefix.lower()) for prefix in LIST_OF_PREFIXES_TO_DROP):
+            if not any(schema_name.lower().startswith(prefix.lower()) for prefix in LIST_OF_EXEMPTIONS):
                 if determine_if_schema_needs_to_be_dropped(schema_name):
                     logger.info(f"Dropping schema name: {schema_name}")
                     try:
-                        data_source_test_helper.data_source_impl.execute_update(
-                            data_source_test_helper.drop_schema_if_exists_sql(
-                                schema_name
-                            )  # Use if exists because we may have multiple CI runs at the same time, removing the same schema.
-                        )
+                        data_source_test_helper.drop_schema_if_exists(schema_name)
+                        num_deleted_schemas += 1
+                        # data_source_test_helper.data_source_impl.execute_update(
+                        #     data_source_test_helper.drop_schema_if_exists_sql(
+                        #         schema_name
+                        #     )  # Use if exists because we may have multiple CI runs at the same time, removing the same schema.
+                        # )
                     except (
                         Exception
                     ) as e:  # We catch any exception, because we don't want the CI itself to fail because of this.
                         logger.error(f"Error dropping schema: {e}")
                 else:
-                    logger.info(f"Schema name: {schema_name} is not old enough to be dropped")
+                    logger.info(f"Schema name: {schema_name} is not old enough to be dropped or is an exemption")
+    logger.info(f"Number of deleted schemas: {num_deleted_schemas}")
